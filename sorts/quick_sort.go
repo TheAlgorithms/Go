5c8180e25ebe42fec0ba96d34d6a6f56197452a6
Kuntal Majumder <hellozee@disroot.org>
2018-10-02 00:29:37 +0530

    Updated Quick Sort to be more elegant and clear

--- sorts/quick_sort.go
+++ sorts/quick_sort.go
@@ -3,51 +3,50 @@ package main
 import "fmt"
 import "math/rand"
 
-
 func quick_sort(arr []int) []int {
-    
-    if len(arr) <= 1 {
-        return arr
-    }
- 
-    median := arr[rand.Intn(len(arr))]
-    
-    low_part := make([]int, 0, len(arr))
-    high_part := make([]int, 0, len(arr))
-    middle_part := make([]int, 0, len(arr))
- 
-    for _, item := range arr {
-        switch {
-        case item < median:
-            low_part = append(low_part, item)
-        case item == median:
-            middle_part = append(middle_part, item)
-        case item > median:
-            high_part = append(high_part, item)
-        }
-    }
- 
-    low_part = quick_sort(low_part)
-    high_part = quick_sort(high_part)
 
-    low_part = append(low_part, middle_part...)
-    low_part = append(low_part, high_part...)
- 
-    return low_part
+	if len(arr) < 2 {
+		return arr
+	}
+
+	// Pick a pivot
+	pivot := rand.Int() % len(arr)
+
+	left, right := 0, len(arr)-1
+
+	// Move the pivot to the right
+	arr[pivot], arr[right] = arr[right], arr[pivot]
+
+	// Pile elements smaller than the pivot on the left
+	for i := range arr {
+		if arr[i] < arr[right] {
+			arr[i], arr[left] = arr[left], arr[i]
+			left++
+		}
+	}
+
+	// Place the pivot after the last smaller element
+	arr[left], arr[right] = arr[right], arr[left]
+
+	//Do this recursively
+	quick_sort(arr[:left])
+	quick_sort(arr[left+1:])
+
+	return arr
 }
 
 func main() {
-    arr := RandomArray(10)
+	arr := RandomArray(10)
 
-    fmt.Println("Initial array is:", arr)
-    fmt.Println("")
-    fmt.Println("Sorted array is: ", quick_sort(arr))
+	fmt.Println("Initial array is:", arr)
+	fmt.Println("")
+	fmt.Println("Sorted array is: ", quick_sort(arr))
 }
 
 func RandomArray(n int) []int {
-    arr := make([]int, n)
-    for i := 0; i <= n - 1; i++ {
-        arr[i] = rand.Intn(n)
-    }
-    return arr
+	arr := make([]int, n)
+	for i := 0; i <= n-1; i++ {
+		arr[i] = rand.Intn(n)
+	}
+	return arr
 }
