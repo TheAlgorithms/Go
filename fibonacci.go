d06e31ef537a6d9b6a9a4bd98485ec313f0dbb91
acueva <acueva@brightcove.com>
2019-10-27 17:25:32 -0600

    Add fibonacci function

---
+++ fibonacci.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func fib() func() int {
+	f1, f2 := 0, 1
+	return func() int {
+		f1, f2 = f2, f1+f2
+		return f1
+	}
+}
+
+func main() {
+	f := fib()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
