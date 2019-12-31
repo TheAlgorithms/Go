65e1463030f89bbfd40813c15866a4eb47410a2a
c4llmeco4ch <cbb692@gmail.com>
2019-12-31 02:20:02 -0800

    Added return type to shell sort

--- sorts/shell_sort.go
+++ sorts/shell_sort.go
@@ -1,7 +1,7 @@
 //Package sorts a package for demonstrating sorting algorithms in Go
 package sorts
 
-func shellSort(arr []int) {
+func shellSort(arr []int) []int {
 	for d := int(len(arr) / 2); d > 0; d /= 2 {
 		for i := d; i < len(arr); i++ {
 			for j := i; j >= d && arr[j-d] > arr[j]; j -= d {
@@ -9,4 +9,5 @@ func shellSort(arr []int) {
 			}
 		}
 	}
+	return arr
 }
