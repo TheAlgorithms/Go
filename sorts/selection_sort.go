d80781bf069eda3ebdc9c3702fd1be1df0361214
c4llmeco4ch <cbb692@gmail.com>
2019-12-31 02:43:53 -0800

    Removed ineffectual assignments in selection sort

--- sorts/selection_sort.go
+++ sorts/selection_sort.go
@@ -3,18 +3,15 @@ package sorts
 
 func selectionSort(arr []int) []int {
 
-	var min int = 0
-	var tmp int = 0
-
 	for i := 0; i < len(arr); i++ {
-		min = i
+		min := i
 		for j := i + 1; j < len(arr); j++ {
 			if arr[j] < arr[min] {
 				min = j
 			}
 		}
 
-		tmp = arr[i]
+		tmp := arr[i]
 		arr[i] = arr[min]
 		arr[min] = tmp
 	}
