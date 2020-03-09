db74531d5ee8091e1c8e41dac43786b056bcfc17
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2020-03-09 14:22:16 +0530

    Update Jump_search.go
    added comments to file.

--- searches/Jump_search.go
+++ searches/Jump_search.go
@@ -7,19 +7,19 @@ func main(){
   i:=0
 	fmt.Print("Enter the Number of Array: ")
 	var n int
-  fmt.Scan(&n)
+  	fmt.Scan(&n)
 	fmt.Println("Enter the elements of array:")
 	arr:=make([]int, n)
 	for(i<n){
-		fmt.Scan(&arr[i])
+		fmt.Scan(&arr[i])			//element should be entered in sorted order
 		i++
 	}
   var jump float64
   var num int
 	fmt.Print("Enter the Number to be Searched: ")
 	fmt.Scan(&num)
-	jump=math.Sqrt(float64(n))
-  i=0
+	jump=math.Sqrt(float64(n))			//initialising value of Jump by find square_root of number of elements in an array
+	i=0
 	for(i<n){
 		if arr[i] == num {
 			fmt.Print("Element found at Location ",i+1)
@@ -28,7 +28,7 @@ func main(){
 			for(arr[i] != num){
 				i=i-1
 			}
-      fmt.Print("Element found at Location ",i+1)
+      fmt.Print("Element found at Location ",i+1)	
 			break
 		}
 		i=i+int(jump)
