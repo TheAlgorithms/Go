ab8acaa3a9135b1bfc7d3a771db9931443384f22
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2020-03-10 20:09:23 +0530

    update binary_search.go

--- searches/binary_search.go
+++ searches/binary_search.go
@@ -3,6 +3,8 @@
 */
 package main
 
+import "fmt"
+
 func binarySearch(array []int, target int, lowIndex int, highIndex int) int {
 	if highIndex < lowIndex {
 		return -1
@@ -32,4 +34,30 @@ func iterBinarySearch(array []int, target int, lowIndex int, highIndex int) int 
 		}
 	}
 	return -1
+}
+func main(){
+	var n,num,ch int
+	fmt.Print("Enter number of Elements in an array:")
+	fmt.Scan(&n)
+	arr:=make([]int, n)
+	i:=0
+	for(i<n){
+		fmt.Print("Enter Element ",i+1,":")
+		fmt.Scan(&arr[i])
+		i=i+1
+	}
+	fmt.Print("Enter the element to be searched:")
+	fmt.Scan(&num)
+	fmt.Println("Enter your choice")
+	fmt.Println("1. Iteration")
+	fmt.Println("2. Recursion")
+	fmt.Scan(&ch)
+	switch ch {
+	case 1:
+		var loc int = iterBinarySearch(arr,num,0,n-1)
+		fmt.Print("Element found at position:",loc+1)
+	case 2:
+		var loc int = binarySearch(arr,num,0,n-1)
+		fmt.Print("Element found at position:",loc+1)
+	}
 }
