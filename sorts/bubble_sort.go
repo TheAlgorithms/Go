13e206ec18e18a9f5cc6b0650b561540f9e70ac9
q <qzqsmile@gmail.com>
2020-05-23 20:24:55 +0800

    update bubblesort swap

--- sorts/bubble_sort.go
+++ sorts/bubble_sort.go
@@ -2,23 +2,15 @@
 package sorts
 
 func bubbleSort(arrayzor []int) []int {
-
-
 	swapped := true
 	for swapped {
 		swapped = false
 		for i := 0; i < len(arrayzor)-1; i++ {
 			if arrayzor[i+1] < arrayzor[i] {
-				swap(arrayzor, i, i+1)
+				arrayzor[i+1], arrayzor[i] =  arrayzor[i], arrayzor[i+1]
 				swapped = true
 			}
 		}
 	}
 	return arrayzor
-}
-
-func swap(arrayzor []int, i, j int) {
-	tmp := arrayzor[j]
-	arrayzor[j] = arrayzor[i]
-	arrayzor[i] = tmp
 }
