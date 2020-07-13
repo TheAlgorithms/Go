2f11c54fbe63d47f312ccd32415a63dcdac5fd68
demetre <durid17@freeuni.edu.ge>
2020-07-13 22:18:41 +0400

    SieveOfEratosthenes added

---
+++ math/SieveOfEratosthenes.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+*gets int n and return all prime numbers before n
+*/
+func SieveOfEratosthenes(n int) []int {
+	isPrime := []bool{}
+	primes := []int{}
+	for i := 0; i <= n; i++{
+		isPrime = append(isPrime, true)
+	}
+	for p := 2; p * p <= n; p++{ 
+        if isPrime[p] {
+            for i := p * p; i <= n; i += p{
+                isPrime[i] = false
+			} 
+        } 
+	}
+
+	for p := 2; p <= n; p++{ 
+		if isPrime[p]{
+			primes = append(primes, p)
+		}
+	}
+	return primes
+}
+
+/*
+func main() {
+	fmt.Println(SieveOfEratosthenes(50))
+}
+*/
