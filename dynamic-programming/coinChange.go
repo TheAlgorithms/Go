e1d4b2222f41d042b58a6cd924ea94280b5ba7b2
Himadri Ganguly <himadri@colourdrift.com>
2020-07-16 03:23:01 +0530

    Fixed lint error.

--- dynamic-programming/coinChange.go
+++ dynamic-programming/coinChange.go
@@ -1,25 +1,29 @@
 /* Coin Change problem
-1. Finding the total number of combination that can be achived with the given number of coins to get the specified sum.
+1. Finding the total number of combination that can be achieved with the given number of coins to get the specified sum.
 */
 
 package main
 
 import "fmt"
 
-func coin_change_count(coins []int, sum int) int {
+func coinChangeCount(coins []int, sum int) int {
 	if sum < 1 {
 		return 0
 	}
+
 	var arr = make([]int, sum+1)
+
 	arr[0] = 1
-	for _, coin_val := range coins {
-		for j := coin_val; j <= sum; j++ {
-			arr[j] += arr[j-coin_val]
+	
+	for _, coinVal := range coins {
+		for j := coinVal; j <= sum; j++ {
+			arr[j] += arr[j-coinVal]
 		}
 	}
+
 	return arr[sum]
 }
 
 func main() {
-        fmt.Println(coin_change_count([]int {1,2}, 4)) // Expected output will be 3
+        fmt.Println(coinChangeCount([]int {1,2}, 4))
 }
