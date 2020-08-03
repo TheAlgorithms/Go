7794f425e2b74b75fbae01780fd1f20d777207fe
byteflow777 <byteflow777@gmail.com>
2020-08-03 15:28:47 +0800

    The bubble sort lack of the function, repair it!

--- sorts/bubble_sort.go
+++ sorts/bubble_sort.go
@@ -1,11 +1,13 @@
 //Package sorts a package for demonstrating sorting algorithms in Go
 package sorts
+
+func bubbleSort(arrayzor []int) []int {
 	swapped := true
 	for swapped {
 		swapped = false
 		for i := 0; i < len(arrayzor)-1; i++ {
 			if arrayzor[i+1] < arrayzor[i] {
-				arrayzor[i+1], arrayzor[i] =  arrayzor[i], arrayzor[i+1]
+				arrayzor[i+1], arrayzor[i] = arrayzor[i], arrayzor[i+1]
 				swapped = true
 			}
 		}
