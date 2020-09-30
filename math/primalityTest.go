23e08ffee813cc38fa11c79191a025c718a863fc
Vedant Mamgain <mamgainvedant@gmail.com>
2020-09-30 23:58:42 +0530

    Create primalityTest.go

---
+++ math/primalityTest.go
@@ -0,0 +1,41 @@
+package main
+
+
+func NaiveApproach(n int) bool {
+	if n==1{
+		return false
+	}
+	for i:=2; i < n ; i++ {
+
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func PairApproach(n int) bool {
+	if n==1{
+		return false
+	}
+	for i:=2;i*i<n;i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+//goos: windows
+//goarch: amd64
+//BenchmarkNaiveApproach-12       660980034                1.81 ns/op
+//BenchmarkPairApproach-12        664641356                1.82 ns/op
+
+
+
+
+
+
+
+
+
