e24d0bb6cf4a3b312fc86cce36fb1f9386fe5326
Vedant Mamgain <mamgainvedant@gmail.com>
2020-10-01 12:39:14 +0530

    Create primalityTest.go

---
+++ math/primalityTest.go
@@ -0,0 +1,36 @@
+//A primality test is an algorithm for determining whether an input number is prime.Among other fields of mathematics, it is used for cryptography.
+//Unlike integer factorization, primality tests do not generally give prime factors, only stating whether the input number is prime or not.
+//Source - Wikipedia https://en.wikipedia.org/wiki/Primality_test
+package main
+
+
+func NaiveApproach(n int) bool {
+	if n==1{
+		return false
+	}
+	for i:=2; i < n ; i++ {
+
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func PairApproach(n int) bool {
+	if n==1{
+		return false
+	}
+	for i:=2;i*i<=n;i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+//goos: windows
+//goarch: amd64
+//BenchmarkNaiveApproach-12       660980034                1.81 ns/op
+//BenchmarkPairApproach-12        664641356                1.82 ns/op
+
