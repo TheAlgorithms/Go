6f4c6359819994807a400d67903712cb22bb31b5
mattan <mattany@gmali.com>
2020-10-07 03:29:57 +0300

    Initial Commit

--- sorts/sorts_test.go
+++ sorts/sorts_test.go
@@ -95,6 +95,19 @@ func TestShell(t *testing.T) {
 	}
 }
 
+func TestRadix(t *testing.T) {
+	for _, test := range sortTests {
+		actual := RadixSort(test.input)
+		pos, sorted := compareSlices(actual, test.expected)
+		if !sorted {
+			if pos == -1 {
+				t.Errorf("test %s failed due to slice length changing", test.name)
+			}
+			t.Errorf("test %s failed at index %d", test.name, pos)
+		}
+	}
+}
+
 /*func TestTopological(t *testing.T) {
 	for _, test := range sortTests {
 		actual := topologicalSort(test.input)
@@ -119,13 +132,13 @@ func BenchmarkBubble(b *testing.B) {
 	}
 }
 
-func BenchmarkSelection(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, test := range sortTests {
-			selectionSort(test.input)
-		}
-	}
-}
+//func BenchmarkSelection(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		for _, test := range sortTests {
+//			selectionSort(test.input)
+//		}
+//	}
+//}
 
 func BenchmarkInsertion(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -163,6 +176,14 @@ func BenchmarkShell(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, test := range sortTests {
 			shellSort(test.input)
+		}
+	}
+}
+
+func BenchmarkRadix(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range sortTests {
+			RadixSort(test.input)
 		}
 	}
 }
