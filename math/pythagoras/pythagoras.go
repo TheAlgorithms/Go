28417f1be3c254b88b964078979e1661c845be0d
localleon <me@lrau.xyz>
2020-10-11 17:13:39 +0200

    Exported Vector Struct for external use

--- math/pythagoras/pythagoras.go
+++ math/pythagoras/pythagoras.go
@@ -6,15 +6,15 @@ import (
 	"math"
 )
 
-// vector defines a tuple with 3 values in 3d-space
-type vector struct {
+//Vector defines a tuple with 3 values in 3d-space
+type Vector struct {
 	x int
 	y int
 	z int
 }
 
 //Distance calculates the distance between to vectors with the   Pythagoras theorem
-func Distance(a, b vector) float64 {
+func Distance(a, b Vector) float64 {
 	abs := float64(math.Abs(float64(b.x - a.x)))
 	res := float64(math.Pow(abs, 2.0) + math.Pow(float64(b.y-a.y), 2.0) + math.Pow(float64(b.z-a.z), 2))
 	fmt.Println(abs, res)
