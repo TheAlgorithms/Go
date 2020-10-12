b9e84b5ede43cbed077e23cb152e4c9abe80c2db
Jobin John <jobin1094@gmail.com>
2020-10-12 21:11:50 +0530

    Files refactored for lint compliance. Tests and benchmarks added.
    Signed-off-by: Jobin John <jobin1094@gmail.com>

--- math/primecheck/prime_check.go
+++ math/primecheck/prime_check.go
@@ -1,8 +1,10 @@
-//A primality test is an algorithm for determining whether an input number is prime.Among other fields of mathematics, it is used for cryptography.
+package primecheck
+
+// A primality test is an algorithm for determining whether an input number is prime.Among other fields of mathematics, it is used for cryptography.
 //Unlike integer factorization, primality tests do not generally give prime factors, only stating whether the input number is prime or not.
 //Source - Wikipedia https://en.wikipedia.org/wiki/Primality_test
-package main
 
+// NaiveApproach checks if an integer is prime or not. Returns a bool.
 func NaiveApproach(n int) bool {
 	if n < 2 {
 		return false
@@ -16,6 +18,7 @@ func NaiveApproach(n int) bool {
 	return true
 }
 
+// PairApproach checks primality of an integer and returns a bool. More efficient than the naive approach as number of iterations are less.
 func PairApproach(n int) bool {
 	if n < 2 {
 		return false
@@ -27,8 +30,3 @@ func PairApproach(n int) bool {
 	}
 	return true
 }
-
-//goos: windows
-//goarch: amd64
-//BenchmarkNaiveApproach-12       660980034                1.81 ns/op
-//BenchmarkPairApproach-12        664641356                1.82 ns/op
