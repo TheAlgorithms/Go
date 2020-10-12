fd6a0655575b089347b233a434700ff5043e97df
Michele Caci <michele.caci@gmail.com>
2020-10-12 07:58:45 +0200

    add tests for xor cipher (#218)
    Co-authored-by: Michele Caci <michele.caci@amadeus.com>

---
+++ ciphers/xor/xor_cipher_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var xorTestData = []struct {
+	description string
+	input       string
+	key         int
+	encrypted   string
+}{
+	{
+		"Encrypt letter 'a' with key 0 makes no changes",
+		"a",
+		0,
+		"a",
+	},
+	{
+		"Encrypt letter 'a' with key 1",
+		"a",
+		1,
+		"`",
+	},
+	{
+		"Encrypt letter 'a' with key 10",
+		"a",
+		10,
+		"k",
+	},
+	{
+		"Encrypt 'hello world' with key 0 makes no changes",
+		"hello world",
+		0,
+		"hello world",
+	},
+	{
+		"Encrypt 'hello world' with key 1",
+		"hello world",
+		1,
+		"idmmn!vnsme",
+	},
+	{
+		"Encrypt 'hello world' with key 10",
+		"hello world",
+		10,
+		"boffe*}exfn",
+	},
+	{
+		"Encrypt full sentence with key 128",
+		"the quick brown fox jumps over the lazy dog.",
+		64,
+		"4(%`15)#+`\"2/7.`&/8`*5-03`/6%2`4(%`,!:9`$/'n",
+	},
+}
+
+func TestXorCipherEncrypt(t *testing.T) {
+	for _, test := range xorTestData {
+		t.Run(test.description, func(t *testing.T) {
+
+			message := toASCII([]rune(test.input))
+			encrypted := encrypt(test.key, message)
+
+			if actual := decodeToString(encrypted); !reflect.DeepEqual(actual, test.encrypted) {
+				t.Logf("FAIL: %s", test.description)
+				t.Fatalf("Expecting %s, actual %s", test.encrypted, actual)
+			}
+		})
+	}
+}
+
+func TestXorCipherDecrypt(t *testing.T) {
+	for _, test := range xorTestData {
+		t.Run(test.description, func(t *testing.T) {
+
+			message := toASCII([]rune(test.encrypted))
+			decrypted := decrypt(test.key, message)
+
+			if actual := decodeToString(decrypted); !reflect.DeepEqual(actual, test.input) {
+				t.Logf("FAIL: %s", test.description)
+				t.Fatalf("Expecting %s, actual %s", test.input, actual)
+			}
+		})
+	}
+}
