8f09e1f2502476a0415c1fd48423f20569af4df5
Michele Caci <michele.caci@gmail.com>
2020-10-12 08:00:00 +0200

    add tests for rsa cipher (#217)
    Co-authored-by: Michele Caci <michele.caci@amadeus.com>

---
+++ ciphers/rsaBig/rsa_cipher_big_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	crypto "crypto/rand"
+	"math/big"
+	"testing"
+)
+
+var rsaTestData = []struct {
+	description string
+	input       string
+}{
+	{
+		"Encrypt letter 'a'",
+		"a",
+	},
+	{
+		"Encrypt 'hello world'",
+		"hello world",
+	},
+	{
+		"Encrypt full sentence",
+		"the quick brown fox jumps over the lazy dog.",
+	},
+	{
+		"Encrypt full sentence from RSAcipher(Big).go main function",
+		"Black Lives Matter, all lives can't matter until Black lives matter",
+	},
+}
+
+func TestRSABigCipher(t *testing.T) {
+	p, _ := crypto.Prime(crypto.Reader, 1024)
+	q, _ := crypto.Prime(crypto.Reader, 1024)
+	n := new(big.Int).Mul(p, q)
+
+	one := big.NewInt(1)
+
+	delta := lcmBig(p.Sub(p, one), q.Sub(q, one))
+
+	e, _ := crypto.Prime(crypto.Reader, delta.BitLen())
+	d := big.NewInt(0)
+	d.ModInverse(e, delta)
+
+	for _, test := range rsaTestData {
+		t.Run(test.description, func(t *testing.T) {
+
+			runes := []rune(test.input)
+			ASCIIs := toASCII(runes)
+			stringEncoded := stringEncode(ASCIIs)
+			bigNum, _ := new(big.Int).SetString(stringEncoded, 0)
+
+			encrypted := encryptBig(bigNum, e, n)
+			decrypted := decryptBig(encrypted, d, n)
+			decryptASCIIs := stringDecode(decrypted)
+
+			if actual := toRune(decryptASCIIs); actual != test.input {
+				t.Logf("FAIL: %s", test.description)
+				t.Fatalf("Expecting %s, actual %s", test.input, actual)
+			}
+		})
+	}
+}
