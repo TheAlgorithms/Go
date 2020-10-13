f7f514afa2e9b412a07a7f3515ff1479051aa001
localleon <me@lrau.xyz>
2020-10-13 14:17:07 +0200

    TableDriven Test, better float64 handling

--- math/pythagoras/pythagoras.go
+++ math/pythagoras/pythagoras.go
@@ -1,22 +1,19 @@
 package pythagoras
 
 import (
-	"fmt"
-
 	"math"
 )
 
 //Vector defines a tuple with 3 values in 3d-space
 type Vector struct {
-	x int
-	y int
-	z int
+	x float64
+	y float64
+	z float64
 }
 
 //Distance calculates the distance between to vectors with the   Pythagoras theorem
 func Distance(a, b Vector) float64 {
-	abs := float64(math.Abs(float64(b.x - a.x)))
-	res := float64(math.Pow(abs, 2.0) + math.Pow(float64(b.y-a.y), 2.0) + math.Pow(float64(b.z-a.z), 2))
-	fmt.Println(abs, res)
+	abs := float64(math.Abs(b.x - a.x))
+	res := float64(math.Pow(abs, 2.0) + math.Pow(b.y-a.y, 2.0) + math.Pow(b.z-a.z, 2.0))
 	return math.Sqrt(res)
 }
--- math/pythagoras/pythagoras_test.go
+++ math/pythagoras/pythagoras_test.go
@@ -1,13 +1,31 @@
 package pythagoras
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+// TableDrivenTest for checking multiple values against our Test Function
+var distanceTest = []struct {
+	v1  Vector
+	v2  Vector
+	res float64
+}{
+	{Vector{2, -1, 7}, Vector{1, -3, 5}, 3.0},
+	{Vector{4, 10, 9}, Vector{4, 3, 5}, 8.06},
+	{Vector{8, 5, 5}, Vector{1, 1, 12}, 10.67},
+	{Vector{1, 1, 1}, Vector{2, 2, 2}, 1.73},
+}
 
 //TestDistance tests the Function Distance with 2 vectors
 func TestDistance(t *testing.T) {
-	v1 := vector{2, -1, 7}
-	v2 := vector{1, -3, 5}
-	dist := Distance(v1, v2)
-	if dist != 3.0 {
-		t.Errorf("Distance was incorrect, got: %f, want: %f.", dist, 3.0)
+	for _, tt := range distanceTest {
+		res := Distance(tt.v1, tt.v2)           // Calculate result for
+		roundRes := (math.Floor(res*100) / 100) // Round to 2 decimal places because we can't compare an infinite number of places
+
+		if roundRes != tt.res {
+			t.Errorf("Distance(%v, %v) = %f, expected %f",
+				tt.v1, tt.v2, roundRes, tt.res)
+		}
 	}
 }
