064854661b163434675249f0315194cd6e1f7006
Adwait Naik <adwaitnaik2@gmail.com>
2020-10-19 22:40:41 +0530

    catalan number

---
+++ other/catalan.go
@@ -0,0 +1,31 @@
+//reference https://mathworld.wolfram.com/CatalanNumber.html
+
+package main
+
+import "fmt"
+
+func factorial(x int)int{
+
+  var  i int
+  var fact int
+  fact = x
+  for i=1; i<x; i++{
+
+    fact= fact * (x-1)
+  
+  }
+  return fact
+}
+
+func main(){
+
+  var n int 
+  var C float64
+  n=6
+  f1:= factorial(2*n)
+  f2:= factorial(n + 1)
+  f3:= factorial(n)
+  C = (float64(f1)/float64(f2*f3))
+  fmt.Printf("%f", C)
+
+}
