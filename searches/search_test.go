f5c0813c551d0af4fb34227315cb0cd80940e28a
Jobin John <jobin1094@gmail.com>
2020-10-25 19:46:11 +0530

    Revert "Update iterBinarySearch (#245)"
    This reverts commit 9087f6789c78caeb1eea7e9fcfa604d106aaa19c.

--- searches/search_test.go
+++ searches/search_test.go
@@ -12,10 +12,6 @@ type searchTest struct {
 var searchTests = []searchTest{
 	//Sanity
 	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, "Sanity"},
-	//First
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, "First"},
-	//Last
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, "Last"},
 	//Absent
 	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, "Absent"},
 	//Empty slice
@@ -26,16 +22,7 @@ func TestBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
 		actual := binarySearch(test.data, test.key, 0, len(test.data)-1)
 		if actual != test.expected {
-			t.Errorf("test %s failed: expected '%d', get '%d'", test.name, test.expected, actual)
-		}
-	}
-}
-
-func TestIterBinarySearch(t *testing.T) {
-	for _, test := range searchTests {
-		actual := iterBinarySearch(test.data, test.key, 0, len(test.data)-1)
-		if actual != test.expected {
-			t.Errorf("test %s failed: expected '%d', get '%d'", test.name, test.expected, actual)
+			t.Errorf("test %s failed", test.name)
 		}
 	}
 }
@@ -44,7 +31,7 @@ func TestLinearSearch(t *testing.T) {
 	for _, test := range searchTests {
 		actual := linearSearch(test.data, test.key)
 		if actual != test.expected {
-			t.Errorf("test %s failed: expected '%d', get '%d'", test.name, test.expected, actual)
+			t.Errorf("test %s failed", test.name)
 		}
 	}
 }
