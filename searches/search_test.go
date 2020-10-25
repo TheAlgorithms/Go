c8471de1789c94932bb4b420c477fa3bf3d85507
Fu Xiang Shu <CrazieElf@hotmail.com>
2020-10-25 21:49:21 +0800

    Added more exhaustive tests and clearer fail message

--- searches/search_test.go
+++ searches/search_test.go
@@ -11,11 +11,16 @@ type searchTest struct {
 
 var searchTests = []searchTest{
 	//Sanity
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 9, 8, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 8, 7, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 7, 6, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 6, 5, "Sanity"},
 	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, "Sanity"},
-	//First
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, "First"},
-	//Last
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, "Last"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 4, 3, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3, 2, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, 1, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, "Sanity"},
 	//Absent
 	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, "Absent"},
 	//Empty slice
@@ -26,7 +31,7 @@ func TestBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
 		actual := binarySearch(test.data, test.key, 0, len(test.data)-1)
 		if actual != test.expected {
-			t.Errorf("test %s failed: expected '%d', get '%d'", test.name, test.expected, actual)
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actual)
 		}
 	}
 }
@@ -35,7 +40,7 @@ func TestIterBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
 		actual := iterBinarySearch(test.data, test.key, 0, len(test.data)-1)
 		if actual != test.expected {
-			t.Errorf("test %s failed: expected '%d', get '%d'", test.name, test.expected, actual)
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actual)
 		}
 	}
 }
@@ -44,7 +49,7 @@ func TestLinearSearch(t *testing.T) {
 	for _, test := range searchTests {
 		actual := linearSearch(test.data, test.key)
 		if actual != test.expected {
-			t.Errorf("test %s failed: expected '%d', get '%d'", test.name, test.expected, actual)
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actual)
 		}
 	}
 }
