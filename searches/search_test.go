596d75e116572ab9af0ef07c01d63b03010a90b7
Fu Xiang Shu <CrazieElf@hotmail.com>
2020-10-25 18:53:09 +0800

    added test for iterBinarySearch and 2 more test cases

--- searches/search_test.go
+++ searches/search_test.go
@@ -12,6 +12,10 @@ type searchTest struct {
 var searchTests = []searchTest{
 	//Sanity
 	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, "Sanity"},
+	//First
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, "First"},
+	//Last
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, "Last"},
 	//Absent
 	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, "Absent"},
 	//Empty slice
@@ -21,6 +25,15 @@ var searchTests = []searchTest{
 func TestBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
 		actual := binarySearch(test.data, test.key, 0, len(test.data)-1)
+		if actual != test.expected {
+			t.Errorf("test %s failed", test.name)
+		}
+	}
+}
+
+func TestIterBinarySearch(t *testing.T) {
+	for _, test := range searchTests {
+		actual := iterBinarySearch(test.data, test.key, 0, len(test.data)-1)
 		if actual != test.expected {
 			t.Errorf("test %s failed", test.name)
 		}
