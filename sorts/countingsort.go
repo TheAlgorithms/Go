35f2bba92daba1569ace997c40fc836136d012b7
Seok Won <alfex4936@gmail.com>
2020-10-25 19:28:21 +0900

    Update counting sort to work with negative numbers

--- sorts/countingsort.go
+++ sorts/countingsort.go
@@ -6,21 +6,27 @@ func countingSort(arr []int) []int {
 	if len(arr) == 0 {
 		return make([]int, 0)
 	}
+	minElement := arr[0]
 	maxElement := arr[0]
 	for i := 1; i < len(arr); i++ {
 		if arr[i] > maxElement {
 			maxElement = arr[i]
 		}
+		if minElement > arr[i] {
+			minElement = arr[i]
+		}
 	}
+
+	maxElement -= minElement
 
 	counts := make([]int, maxElement+1)
 	result := make([]int, 0)
 	for _, item := range arr {
-		counts[item]++
+		counts[item-minElement]++
 	}
 	for i := 0; i <= maxElement; i++ {
 		for j := 0; j < counts[i]; j++ {
-			result = append(result, i)
+			result = append(result, i+minElement)
 		}
 	}
 	return result
