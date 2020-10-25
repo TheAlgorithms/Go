efce802864720cc6430c87468ee376bc4d77726a
Seok Won <alfex4936@gmail.com>
2020-10-25 19:34:57 +0900

    Added quickSort with hoare partition scheme
    Tail recursive + Hoare partition scheme (centered pivot)
    
    It was the fastest quick sort variation among quick sort recursive, quic sort random pivot and quick sort median of three killer when I did benchmarks with those. (didn't use insertion sort for smaller parts)
    
    Quick Sort using recursive: 2.821s
    Quick Sort using Hoare: 1.961s

--- sorts/quick_sort.go
+++ sorts/quick_sort.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 )
 
+// QuickSort using random pivot
 func QuickSort(arr []int) []int {
 
 	if len(arr) <= 1 {
@@ -35,4 +36,45 @@ func QuickSort(arr []int) []int {
 	lowPart = append(lowPart, highPart...)
 
 	return lowPart
+}
+
+func partition(arr []int, lo int, hi int) int {
+	i := lo - 1
+	j := hi + 1
+	pivot := arr[(lo+hi)/2]
+
+	for true {
+		i++
+		for arr[i] < pivot {
+			i++
+		}
+		j--
+		for arr[j] > pivot {
+			j--
+		}
+
+		if i >= j {
+			return j
+		}
+
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+
+	// unreachable
+	return j
+}
+
+func QuickSortHoareHelper(arr []int, lo int, hi int) []int {
+	for hi > lo {
+		p := partition(arr, lo, hi)
+		QuickSortHoareHelper(arr, lo, p)
+		lo = p + 1
+	}
+
+	return arr
+}
+
+// QuickSortHoare using Hoare partition scheme and centered pivot
+func QuickSortHoare(arr []int) []int {
+	return QuickSortHoareHelper(arr, 0, len(arr)-1)
 }
