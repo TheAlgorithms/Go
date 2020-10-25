da5811dd2e7a155405ac39b287baa607d4275401
Seok Won <alfex4936@gmail.com>
2020-10-25 19:39:20 +0900

    Update Selection Sort
    We don't have to iterate until last element, because the nested loop doesn't need it.

--- sorts/selection_sort.go
+++ sorts/selection_sort.go
@@ -3,7 +3,7 @@ package sorts
 
 func SelectionSort(arr []int) []int {
 
-	for i := 0; i < len(arr); i++ {
+	for i := 0; i < len(arr)-1; i++ {
 		min := i
 		for j := i + 1; j < len(arr); j++ {
 			if arr[j] < arr[min] {
