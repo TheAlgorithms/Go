2a71f7b7cdd089ea6967e8533c5beb289117a5f4
Vedant Mamgain <mamgainvedant@gmail.com>
2021-01-17 12:13:10 +0530

    Fix Ceaser Cipher

--- ciphers/caesar/CaesarCipher.go
+++ ciphers/caesar/CaesarCipher.go
@@ -1,40 +1,40 @@
-package main
-
+package ceaser
+ 
 import (
 	"bytes"
 	"flag"
 	"fmt"
 	"strings"
 )
-
+ 
 func main() {
 	cipherKey := flag.Int("c", 0, "Cipher shift amount (-26 - 26)")
 	input := flag.String("i", "", "Input")
 	flag.Parse()
-
+ 
 	if *cipherKey > 26 || *cipherKey < -26 {
 		flag.PrintDefaults()
 	} else {
 		fmt.Println(caesarCipher(*input, *cipherKey))
 	}
-
+ 
 }
-
+ 
 func caesarCipher(input string, key int) string {
 	var outputBuffer bytes.Buffer
 	for _, r := range strings.ToLower(input) {
-		newByte := int(r)
-
+		newByte := byte(r)
+ 
 		if newByte >= 'a' && newByte <= 'z' {
-			newByte += key
-
+			newByte += byte(key)
+ 
 			if newByte > 'z' {
 				newByte -= 26
 			} else if newByte < 'a' {
 				newByte += 26
-			}
+			}	
 		}
-
+ 
 		outputBuffer.WriteString(string(newByte))
 	}
 	return outputBuffer.String()
--- ciphers/caesar/caesar_test.go
+++ ciphers/caesar/caesar_test.go
@@ -1,4 +1,4 @@
-package main
+package ceaser
 
 import (
 	"testing"
