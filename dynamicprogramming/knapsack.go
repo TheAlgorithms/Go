ead991b4d04d04c153dba0ff36244570a9f1da1c
Tajmeet Singh <tjgurwara99@gmail.com>
2021-02-17 19:01:17 +0000

    fix: automated errors in dynamicprogramming with golangci-lint

--- dynamicprogramming/knapsack.go
+++ dynamicprogramming/knapsack.go
@@ -12,9 +12,9 @@ func Max(a, b int) int {
 	return int(math.Max(float64(a), float64(b)))
 }
 
-// solve knapsack problem
+// Solve knapsack problem
 // return maxProfit
-func solve(maxWeight int, weights, values []int) int {
+func Solve(maxWeight int, weights, values []int) int {
 	n := len(weights)
 	m := maxWeight
 	// create dp data structure
