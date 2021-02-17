099a88d15d47266588163337b3da2576969994c5
Tajmeet Singh <tjgurwara99@gmail.com>
2021-02-17 05:18:06 +0000

    fix: Tried to correct the implemention so that the redeclaration error that appears in tests goes away. TODO: Maybe implement it with interfaces.

--- datastructures/binary-tree/binarytree.go
+++ datastructures/binary-tree/binarytree.go
@@ -1,88 +1,12 @@
-// basic binary tree and related operations
-
+// Package binarytree basic binary tree and related operations
 package binarytree
 
-// package main
-
-import "fmt"
-
-type node struct {
-	val   int
-	left  *node
-	right *node
-}
-
-type btree struct {
-	root *node
-}
-
-func max(a, b int) int {
+// Max Function that returns max of two numbers - possibly already declared.
+func Max(a, b int) int {
 	if a > b {
 		return a
 	}
 	return b
-}
-
-func newNode(val int) *node {
-	n := &node{val, nil, nil}
-	return n
-}
-
-func inorder(n *node) {
-	if n == nil {
-		return
-	}
-	inorder(n.left)
-	fmt.Print(n.val, " ")
-	inorder(n.right)
-}
-
-func preorder(n *node) {
-	if n == nil {
-		return
-	}
-	fmt.Print(n.val, " ")
-	preorder(n.left)
-	preorder(n.right)
-}
-
-func postorder(n *node) {
-	if n == nil {
-		return
-	}
-	postorder(n.left)
-	postorder(n.right)
-	fmt.Print(n.val, " ")
-}
-
-func levelorder(root *node) {
-	var q []*node // queue
-	var n *node   // temporary node
-
-	q = append(q, root)
-
-	for len(q) != 0 {
-		n, q = q[0], q[1:]
-		fmt.Print(n.val, " ")
-		if n.left != nil {
-			q = append(q, n.left)
-		}
-		if n.right != nil {
-			q = append(q, n.right)
-		}
-	}
-}
-
-// helper function for t.depth
-func _calculate_depth(n *node, depth int) int {
-	if n == nil {
-		return depth
-	}
-	return max(_calculate_depth(n.left, depth+1), _calculate_depth(n.right, depth+1))
-}
-
-func (t *btree) depth() int {
-	return _calculate_depth(t.root, 0)
 }
 
 /*
