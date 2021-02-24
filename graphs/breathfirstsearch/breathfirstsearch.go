2eeedd3e452ccbaddd0ff16b8f931d694dd739d0
kaveh <kaveh.fazaeli@gmail.com>
2021-02-24 11:19:52 +0330

    Revise BFS algorithm implementation.

--- graphs/breathfirstsearch/breathfirstsearch.go
+++ graphs/breathfirstsearch/breathfirstsearch.go
@@ -1,73 +1,31 @@
 package breathfirstsearch
 
-// GetIdx ...
-func GetIdx(target int, nodes []int) int {
-	for i := 0; i < len(nodes); i++ {
-		if nodes[i] == target {
-			return i
-		}
-	}
-	return -1
-}
+/*
 
-// NotExist ...
-func NotExist(target int, slice []int) bool {
-	for i := 0; i < len(slice); i++ {
-		if slice[i] == target {
-			return false
-		}
-	}
-	return true
-}
+	Breadth First Search
+	https://en.wikipedia.org/wiki/Breadth-first_search
 
-// BreadthFirstSearch The main algorithm for bread first search
-func BreadthFirstSearch(start, end int, nodes []int, edges [][]bool) bool {
-	var route []int
-	var queue []int
-	startIdx := GetIdx(start, nodes)
-	queue = append(queue, startIdx)
+*/
+
+func BreadthFirstSearch(start, end, nodes int, edges [][]int) (isConnected bool, distance int) {
+	queue := make([]int, 0)
+	discovered := make([]int, nodes)
+	discovered[start] = 1
+	queue = append(queue, start)
 	for len(queue) > 0 {
-		now := queue[0]
-		route = append(route, nodes[now])
-		if len(queue) > 1 {
+		v := queue[0]
+		if len(queue) > 0 {
 			queue = queue[1:]
-		} else {
-			queue = queue[0:]
 		}
-		for i := 0; i < len(edges[now]); i++ {
-			if edges[now][i] && NotExist(i, queue) {
+		for i := 0; i < len(edges[v]); i++ {
+			if discovered[i] == 0 && edges[v][i] > 0 {
+				if i == end {
+					return true, discovered[v]
+				}
+				discovered[i] = discovered[v] + 1
 				queue = append(queue, i)
 			}
-			edges[now][i] = false
-			edges[i][now] = false
-		}
-		if route[len(route)-1] == end {
-			return true
 		}
 	}
-	return false
+	return false, 0
 }
-
-// func main() {
-// 	nodes := []int{
-// 		1, 2, 3, 4, 5, 6,
-// 	}
-// 	/*
-// 		sample graph
-// 		①-②
-// 		|  |
-// 		③-④-⑤-⑥
-// 	*/
-// 	edges := [][]bool{
-// 		{false, true, true, false, false, false},
-// 		{true, false, false, true, false, false},
-// 		{true, false, false, true, false, false},
-// 		{false, true, true, false, true, false},
-// 		{false, false, false, true, false, true},
-// 		{false, false, false, false, true, false},
-// 	}
-// 	start := 1
-// 	end := 6
-// 	result := breadthFirstSearch(start, end, nodes, edges)
-// 	fmt.Println(result)
-// }
