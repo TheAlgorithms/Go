af9837cccc4371bd5133393f5cb4ed9d765592fd
AmaterasuOmikami <amaterasuom1k4m1@gmail.com>
2021-03-07 19:42:47 +0700

    add Linked Cycle List struct, test coverage 100%

---
+++ datastructures/linkedlist/cyclicallylinkedlist/cyclicallylinkedlist.go
@@ -0,0 +1,139 @@
+// Package cyclicallylinkedlist demonstration of Linked List Cycle in golang
+
+package cyclicallylinkedlist
+
+import "fmt"
+
+type ClNode struct {
+	val  interface{}
+	prev *ClNode
+	next *ClNode
+}
+
+// The Cycling list in this implementation is addressed
+// by means of the element located at the current position.
+type ClList struct {
+	size        int
+	currentItem *ClNode
+}
+
+func NewList() *ClList {
+	return &ClList{0, nil}
+}
+
+func NewNode(val interface{}) *ClNode {
+	return &ClNode{val, nil, nil}
+}
+
+// Inserting the first node is a special case. It will
+// point to itself. For other cases, the node will be added
+// to the end of the list. Enf of the list is prev field of
+// current item. Complexity O(1).
+func (cl *ClList) Add(val interface{}) {
+	n := NewNode(val)
+	cl.size++
+	if cl.currentItem == nil {
+		n.prev = n
+		n.next = n
+		cl.currentItem = n
+	} else {
+		n.prev = cl.currentItem.prev
+		n.next = cl.currentItem
+		cl.currentItem.prev.next = n
+		cl.currentItem.prev = n
+	}
+}
+
+// Rotate method is interesting for optimization.
+// Let us need to rotate the list by P places
+// and list have N items. For first optimization
+// we must decrease P value so that is in range from
+// 0 to N-1. For this we need to use the operation of
+// division modulo. But be careful if P is less than 0.
+// if it is - make it positive. This can be done without
+// violating the meaning of the number by adding to it
+// a multiple of N. Now you can decrease P modulo H to
+// rotate the list by the minimum number of places.
+// We use the fact that moving forward in a circle by P
+// places is the same as moving N - P places back.
+// Therefore, if P> N / 2, you can turn the list by N-P places back.
+// Complexity O(n).
+func (cl *ClList) Rotate(places int) {
+	if cl.size > 0 {
+		if places < 0 {
+			multiple := cl.size - 1 - places/cl.size
+			places += multiple * cl.size
+		}
+		places %= cl.size
+
+		if places > cl.size/2 {
+			places = cl.size - places
+			for i := 0; i < places; i++ {
+				cl.currentItem = cl.currentItem.prev
+			}
+		} else if places == 0 {
+			return
+		} else {
+			for i := 0; i < places; i++ {
+				cl.currentItem = cl.currentItem.next
+			}
+
+		}
+	}
+}
+
+// Delete the current item.
+func (cl *ClList) Delete() int {
+
+	var deleted int
+	var prevItem, thisItem, nextItem *ClNode
+
+	if cl.size > 0 {
+		deleted = 1
+		thisItem = cl.currentItem
+		nextItem = thisItem.next
+		prevItem = thisItem.prev
+
+		if cl.size == 1 {
+			cl.currentItem = nil
+		} else {
+			cl.currentItem = nextItem
+			nextItem.prev = prevItem
+			prevItem.next = nextItem
+		}
+		cl.size--
+	}
+	return deleted
+}
+
+// Destroy all items in the list.
+func (cl *ClList) Destroy() {
+	for cl.Delete() == 1 {
+		continue
+	}
+}
+
+// Show list body.
+func (cl *ClList) Walk() *ClNode {
+	var start *ClNode
+	start = cl.currentItem
+
+	for i := 0; i < cl.size; i++ {
+		fmt.Printf("%v \n", start.val)
+		start = start.next
+	}
+	return start
+}
+
+// https://en.wikipedia.org/wiki/Josephus_problem
+// This is a struct-based solution for Josephus problem.
+func JosephusProblem(cl *ClList, k int) int {
+	for cl.size > 1 {
+		cl.Rotate(k)
+		cl.Delete()
+		cl.Rotate(-1)
+	}
+	retval := cl.currentItem.val.(int)
+	cl.Destroy()
+	return retval
+}
---
+++ datastructures/linkedlist/cyclicallylinkedlist/cyclicallylinkedlist_test.go
@@ -0,0 +1,108 @@
+package cyclicallylinkedlist
+
+import (
+	"reflect"
+	"testing"
+)
+
+type testCase struct {
+	param        int
+	wantToReturn int
+	listCount    int
+}
+
+func fillList(list *ClList, n int) {
+	for i := 1; i <= n; i++ {
+		list.Add(i)
+	}
+}
+
+// 100% coverage.
+func TestLinkedList(t *testing.T) {
+	list := NewList()
+	list.Add(1)
+	list.Add(2)
+	list.Add(3)
+
+	t.Run("Test Add()", func(t *testing.T) {
+		want := []interface{}{1, 2, 3}
+		var got []interface{}
+		var start *ClNode
+		start = list.currentItem
+
+		for i := 0; i < list.size; i++ {
+			got = append(got, start.val)
+			start = start.next
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+
+	t.Run("Test Walk()", func(t *testing.T) {
+		want := 1
+		got := list.Walk()
+
+		if got.val != want {
+			t.Errorf("got: %v, want: nil", got)
+		}
+	})
+
+	t.Run("Test Rotate()", func(t *testing.T) {
+		testCases := []testCase{
+			{1, 2, 0},
+			{3, 2, 0},
+			{6, 2, 0},
+			{7, 3, 0},
+			{-2, 1, 0},
+			{5, 3, 0},
+			{8, 2, 0},
+			{-8, 3, 0},
+		}
+		for idx, tCase := range testCases {
+			list.Rotate(tCase.param)
+			got := list.currentItem.val
+			if got != tCase.wantToReturn {
+				t.Errorf("got: %v, want: %v for test id %v", got, tCase.wantToReturn, idx)
+			}
+		}
+
+	})
+
+	t.Run("Test Delete()", func(t *testing.T) {
+		want := 1
+		list.Delete()
+		got := list.currentItem.val
+		if want != got {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+
+	t.Run("Test Destroy()", func(t *testing.T) {
+		list.Destroy()
+		got := list.currentItem
+		if got != nil {
+			t.Errorf("got: %v, want: nil", got)
+		}
+	})
+
+	t.Run("Test JosephTask()", func(t *testing.T) {
+		testCases := []testCase{
+			{5, 4, 8},
+			{3, 8, 8},
+			{8, 5, 8},
+			{8, 5, 8},
+			{2, 14, 14},
+			{13, 56, 58},
+			{7, 5, 5},
+		}
+		for _, tCase := range testCases {
+			list.Destroy()
+			fillList(list, tCase.listCount)
+			got := JosephusProblem(list, tCase.param)
+			if got != tCase.wantToReturn {
+				t.Errorf("got: %v, want: %v", got, tCase.wantToReturn)
+			}
+		}
+	})
+}
