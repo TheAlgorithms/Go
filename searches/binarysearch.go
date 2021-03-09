a95272fde76b1eadac44668abe1336e62961a51a
FX Shu <crazieelf@hotmail.com>
2021-03-09 13:24:20 +0800

    Added error for when target not found

--- searches/binarysearch.go
+++ searches/binarysearch.go
@@ -1,9 +1,13 @@
 package searches
 
+import "errors"
+
+var errNotFound = errors.New("target not found in array")
+
 // BinarySearch Binary Search
-func BinarySearch(array []int, target int, lowIndex int, highIndex int) int {
+func BinarySearch(array []int, target int, lowIndex int, highIndex int) (int, error) {
 	if highIndex < lowIndex || len(array) == 0 {
-		return -1
+		return -1, errNotFound
 	}
 	mid := int(lowIndex + (highIndex-lowIndex)/2)
 	if array[mid] > target {
@@ -11,12 +15,12 @@ func BinarySearch(array []int, target int, lowIndex int, highIndex int) int {
 	} else if array[mid] < target {
 		return BinarySearch(array, target, mid+1, highIndex)
 	} else {
-		return mid
+		return mid, nil
 	}
 }
 
 // IterBinarySearch Iter Binary Search
-func IterBinarySearch(array []int, target int, lowIndex int, highIndex int) int {
+func IterBinarySearch(array []int, target int, lowIndex int, highIndex int) (int, error) {
 	startIndex := lowIndex
 	endIndex := highIndex
 	var mid int
@@ -27,8 +31,8 @@ func IterBinarySearch(array []int, target int, lowIndex int, highIndex int) int 
 		} else if array[mid] < target {
 			startIndex = mid + 1
 		} else {
-			return mid
+			return mid, nil
 		}
 	}
-	return -1
+	return -1, errNotFound
 }
--- searches/search_test.go
+++ searches/search_test.go
@@ -1,47 +1,56 @@
 package searches
 
-import "testing"
+import (
+	"testing"
+)
 
 type searchTest struct {
-	data     []int
-	key      int
-	expected int
-	name     string
+	data          []int
+	key           int
+	expectedValue int
+	expectedError error
+	name          string
 }
 
 var searchTests = []searchTest{
 	//Sanity
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 9, 8, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 8, 7, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 7, 6, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 6, 5, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 4, 3, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3, 2, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, 1, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 9, 8, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 8, 7, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 7, 6, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 6, 5, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 4, 3, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3, 2, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, 1, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, nil, "Sanity"},
 	//Absent
-	{[]int{1, 4, 5, 6, 7, 10}, -25, -1, "Absent"},
-	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, "Absent"},
+	{[]int{1, 4, 5, 6, 7, 10}, -25, -1, errNotFound, "Absent"},
+	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, errNotFound, "Absent"},
 	//Empty slice
-	{[]int{}, 2, -1, "Empty"},
+	{[]int{}, 2, -1, errNotFound, "Empty"},
 }
 
 func TestBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
-		actual := BinarySearch(test.data, test.key, 0, len(test.data)-1)
-		if actual != test.expected {
-			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actual)
+		actualValue, actualError := BinarySearch(test.data, test.key, 0, len(test.data)-1)
+		if actualValue != test.expectedValue {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expectedValue, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
 		}
 	}
 }
 
 func TestIterBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
-		actual := IterBinarySearch(test.data, test.key, 0, len(test.data)-1)
-		if actual != test.expected {
-			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actual)
+		actualValue, actualError := IterBinarySearch(test.data, test.key, 0, len(test.data)-1)
+		if actualValue != test.expectedValue {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expectedValue, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
 		}
 	}
 }
@@ -49,8 +58,8 @@ func TestIterBinarySearch(t *testing.T) {
 func TestLinearSearch(t *testing.T) {
 	for _, test := range searchTests {
 		actual := LinearSearch(test.data, test.key)
-		if actual != test.expected {
-			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actual)
+		if actual != test.expectedValue {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expectedValue, actual)
 		}
 	}
 }
