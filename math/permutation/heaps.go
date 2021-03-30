ac5590df6e12864d54d849d319978ecb40ac37e6
Kevin Burchfield <kevinburchfi@gmail.com>
2021-03-29 22:15:05 -0400

    Fix lint

--- math/permutation/heaps.go
+++ math/permutation/heaps.go
@@ -36,7 +36,7 @@ func Heaps(out chan []string, n int) {
 
 func GenerateElementSet(out chan []string, n int) {
 	elementSet := make([]string, n)
-	for i, _ := range elementSet {
+	for i := range elementSet {
 		elementSet[i] = string(i + 49) // Adjust this if you want to change your charset
 	}
 	out <- elementSet
