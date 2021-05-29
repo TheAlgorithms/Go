7b21dbe95bdef89868fa7eef586332bf1526e6d7
Patricio Whittingslow <graded.sp@gmail.com>
2021-05-29 16:24:27 -0300

    roman_to_int remove string casting
    easier to read without string casting all over the place

--- conversions/roman-to-integer/roman_to_integer.go
+++ conversions/roman-to-integer/roman_to_integer.go
@@ -7,17 +7,17 @@ Only standard form is supported
 */
 package conversions
 
-var romans = map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+var romans = map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 func RomanToInteger(roman string) int {
 	total := 0
 	holder := 0
 	for holder < len(roman) {
-		if holder+1 < len(roman) && (romans[string(roman[holder])] < romans[string(roman[holder+1])]) {
-			total += romans[string(roman[holder+1])] - romans[string(roman[holder])]
+		if holder+1 < len(roman) && (romans[roman[holder]] < romans[roman[holder+1]]) {
+			total += romans[roman[holder+1]] - romans[roman[holder]]
 			holder += 2
 		} else {
-			total += romans[string(roman[holder])]
+			total += romans[roman[holder]]
 			holder++
 		}
 	}
