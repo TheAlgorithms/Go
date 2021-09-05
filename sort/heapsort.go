2908a2a632cc54c96d76749b9930f9515bb52926
Rak Laptudirm <raklaptudirm@gmail.com>
2021-09-05 14:30:26 +0530

    chore: remove commented code

--- sort/heapsort.go
+++ sort/heapsort.go
@@ -23,7 +23,6 @@ func (h maxHeap) MaxHeapify(i int) {
 	if r < h.size() && h.slice[r] > h.slice[max] {
 		max = r
 	}
-	//log.Printf("MaxHeapify(%v): l,r=%v,%v; max=%v\t%v\n", i, l, r, max, h.slice)
 	if max != i {
 		h.slice[i], h.slice[max] = h.slice[max], h.slice[i]
 		h.MaxHeapify(max)
@@ -34,17 +33,10 @@ func (h maxHeap) size() int { return h.heapSize } // ???
 
 func HeapSort(slice []int) []int {
 	h := buildMaxHeap(slice)
-	//log.Println(slice)
 	for i := len(h.slice) - 1; i >= 1; i-- {
 		h.slice[0], h.slice[i] = h.slice[i], h.slice[0]
 		h.heapSize--
 		h.MaxHeapify(0)
-		/*if i == len(h.slice)-1 || i == len(h.slice)-3 || i == len(h.slice)-5 {
-			element := (i - len(h.slice)) * -1
-			fmt.Println("Heap after removing ", element, " elements")
-			fmt.Println(h.slice)
-
-		}*/
 	}
 	return h.slice
 }
