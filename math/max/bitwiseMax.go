f16c2e7696ec2829b65ecd72ec4b36ebbbe89c4e
Ilya Sokolov <developer.sokolov@gmail.com>
2021-09-17 12:16:20 +0300

    fix: rename to bitwiseMax

--- math/max/bitwiseMax.go
+++ math/max/bitwiseMax.go
@@ -1,13 +1,13 @@
-// max2.go
+// bitwiseMax.go
 // description: Gives max of two integers
 // details:
 // implementation of finding the maximum of two numbers using only binary operations without using conditions
 // author(s) [red_byte](https://github.com/i-redbyte)
-// see max2_test.go
+// see bitwiseMax_test.go
 
 package max
 
-func Max2(a int, b int, base int) int {
+func BitwiseMax(a int, b int, base int) int {
 	z := a - b
 	i := (z >> base) & 1
 	return a - (i * z)
--- math/max/bitwiseMax_test.go
+++ math/max/bitwiseMax_test.go
@@ -1,77 +1,77 @@
-// max2_test.go
-// description: Test for Max2
+// bitwiseMax_test.go
+// description: Test for BitwiseMax
 // author(s) [red_byte](https://github.com/i-redbyte)
-// see max2.go
+// see bitwiseMax.go
 
 package max
 
 import "testing"
 
-func TestMax2(t *testing.T) {
+func TestBitwiseMax(t *testing.T) {
 	base32 := 31
 
 	t.Run("Testing(32bit) a = 32 and m = 64: ", func(t *testing.T) {
-		max := Max2(32, 64, base32)
+		max := BitwiseMax(32, 64, base32)
 		if max != 64 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	t.Run("Testing(32bit) a = 1024 and m = -9: ", func(t *testing.T) {
-		max := Max2(1024, -9, base32)
+		max := BitwiseMax(1024, -9, base32)
 		if max != 1024 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	t.Run("Testing(32bit) a = -6 and m = -6: ", func(t *testing.T) {
-		max := Max2(-6, -6, base32)
+		max := BitwiseMax(-6, -6, base32)
 		if max != -6 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	t.Run("Testing(32bit) a = -72 and m = -73: ", func(t *testing.T) {
-		max := Max2(-72, -73, base32)
+		max := BitwiseMax(-72, -73, base32)
 		if max != -72 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	base64 := 63
 	t.Run("Testing(64bit) a = 32 and m = 9223372036854775807: ", func(t *testing.T) {
-		max := Max2(32, 9223372036854775807, base64)
+		max := BitwiseMax(32, 9223372036854775807, base64)
 		if max != 9223372036854775807 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	t.Run("Testing(64bit) a = 1024 and m = -9223372036854770001: ", func(t *testing.T) {
-		max := Max2(1024, -9223372036854770001, base64)
+		max := BitwiseMax(1024, -9223372036854770001, base64)
 		if max != 1024 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	t.Run("Testing(64bit) a = -6 and m = -6: ", func(t *testing.T) {
-		max := Max2(-6, -6, base64)
+		max := BitwiseMax(-6, -6, base64)
 		if max != -6 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	t.Run("Testing(64bit) a = -4223372036854775809 and m = -4223372036854775808: ", func(t *testing.T) {
-		max := Max2(-4223372036854775809, -4223372036854775808, base64)
+		max := BitwiseMax(-4223372036854775809, -4223372036854775808, base64)
 		if max != -4223372036854775808 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 
 	base8 := 7
 	t.Run("Testing(8bit) a = 257 and m = 256: ", func(t *testing.T) {
-		max := Max2(8, 16, base8)
+		max := BitwiseMax(8, 16, base8)
 		if max != 16 {
-			t.Fatalf("Error: Max2 returned bad value")
+			t.Fatalf("Error: BitwiseMax returned bad value")
 		}
 	})
 }
