7fd902f490039da4af5e26c140989ba95a4d4958
FX Shu <crazieelf@hotmail.com>
2021-09-20 11:21:25 +0800

    Updated test and added benchmark

--- search/binary_test.go
+++ search/binary_test.go
@@ -1,23 +1,45 @@
 package search
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
-		actual := Binary(test.data, test.key, 0, len(test.data)-1)
-		if actual != test.expected {
-			t.Errorf("test %s failed", test.name)
+		actualValue, _ := Binary(test.data, test.key, 0, len(test.data)-1)
+		if actualValue != test.expected {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
 		}
 	}
 }
 
 func TestIterBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
-		actual := BinaryIterative(test.data, test.key, 0, len(test.data)-1)
-		if actual != test.expected {
-			t.Errorf("test %s failed", test.name)
+		actualValue, _ := BinaryIterative(test.data, test.key, 0, len(test.data)-1)
+		if actualValue != test.expected {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
 		}
+	}
+}
+
+func generateBenchmarkTestCase() []int {
+	var testCase []int
+	for i := 0; i < 100; i++ {
+		testCase = append(testCase, i)
+	}
+	return testCase
+}
+
+func BenchmarkBinarySearch(b *testing.B) {
+	testCase := generateBenchmarkTestCase()
+	b.ResetTimer() // this is important because the generateBenchmarkTestCase() is expensive
+	for i := 0; i < b.N; i++ {
+		_, _ = Binary(testCase, 10, 0, len(testCase)-1)
+	}
+}
+
+func BenchmarkIterBinarySearch(b *testing.B) {
+	testCase := generateBenchmarkTestCase()
+	b.ResetTimer() // this is important because the generateBenchmarkTestCase() is expensive
+	for i := 0; i < b.N; i++ {
+		_, _ = BinaryIterative(testCase, 10, 0, len(testCase)-1)
 	}
 }
