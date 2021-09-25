76dfa95f1972c23851f972e32a4d18345ad77245
Femi Novia Lina <feminovi@gmail.com>
2021-09-25 20:58:02 +0700

    add btree test file

---
+++ structure/binarytree/btree_test.go
@@ -0,0 +1,147 @@
+package binarytree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	BTree := BTree{
+		Root: NewNode(90),
+	}
+
+	root := BTree.Root
+
+	Insert(root, 80)
+	Insert(root, 100)
+
+	if root.val != 90 {
+		t.Errorf("root should have value = 90")
+	}
+
+	if root.left.val != 80 {
+		t.Errorf("left child should have value = 80")
+	}
+
+	if root.right.val != 100 {
+		t.Errorf("right child should have value = 100")
+	}
+
+	if BTree.Depth() != 2 {
+		t.Errorf("tree should have depth = 1")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("Delete a node with no child", func(t *testing.T) {
+		BTree := BTree{
+			Root: NewNode(90),
+		}
+
+		root := BTree.Root
+
+		Insert(root, 80)
+		Insert(root, 100)
+
+		BstDelete(root, 100)
+
+		if root.val != 90 {
+			t.Errorf("root should have value = 90")
+		}
+
+		if root.left.val != 80 {
+			t.Errorf("left child should have value = 80")
+		}
+
+		if root.right != nil {
+			t.Errorf("right child should have value = nil")
+		}
+
+		if BTree.Depth() != 2 {
+			t.Errorf("Depth should have value = 2")
+		}
+	})
+
+	t.Run("Delete a node with one child", func(t *testing.T) {
+		BTree := BTree{
+			Root: NewNode(90),
+		}
+
+		root := BTree.Root
+
+		Insert(root, 80)
+		Insert(root, 100)
+		Insert(root, 70)
+
+		BstDelete(root, 80)
+
+		if root.val != 90 {
+			t.Errorf("root should have value = 90")
+		}
+
+		if root.right.val != 100 {
+			t.Errorf("right child should have value = 100")
+		}
+
+		if root.left.val != 70 {
+			t.Errorf("left child should have value = 70")
+		}
+
+		if BTree.Depth() != 2 {
+			t.Errorf("Depth should have value = 2")
+		}
+	})
+
+	t.Run("Delete a node with two children", func(t *testing.T) {
+		BTree := BTree{
+			Root: NewNode(90),
+		}
+
+		root := BTree.Root
+
+		Insert(root, 80)
+		Insert(root, 100)
+		Insert(root, 70)
+		Insert(root, 85)
+
+		BstDelete(root, 80)
+
+		if root.val != 90 {
+			t.Errorf("root should have value = 90")
+		}
+
+		if root.left.val != 85 {
+			t.Errorf("left child should have value = 85")
+		}
+
+		if root.right.val != 100 {
+			t.Errorf("right child should have value = 100")
+		}
+
+		if BTree.Depth() != 3 {
+			t.Errorf("Depth should have value = 3")
+		}
+	})
+}
+
+func TestAccessNodesByLayer(t *testing.T) {
+	BTree := BTree{
+		Root: NewNode(90),
+	}
+
+	root := BTree.Root
+
+	Insert(root, 80)
+	Insert(root, 100)
+	Insert(root, 70)
+	Insert(root, 85)
+	Insert(root, 95)
+	Insert(root, 105)
+
+	a := AccessNodesByLayer(root)
+	b := [][]int{{90}, {80, 100}, {70, 85, 95, 105}}
+
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("Nodes should have value = [[90] [80 100] [70 85 95 105]]")
+	}
+}
