43f3ad983d4f70b15eab4554ca18decdfa1e8e55
FX Shu <crazieelf@hotmail.com>
2021-09-27 16:57:45 +0800

    added test cases and added tests for error

--- search/binary_test.go
+++ search/binary_test.go
@@ -4,18 +4,24 @@ import "testing"
 
 func TestBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
-		actualValue, _ := Binary(test.data, test.key, 0, len(test.data)-1)
+		actualValue, actualError := Binary(test.data, test.key, 0, len(test.data)-1)
 		if actualValue != test.expected {
 			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
 		}
 	}
 }
 
 func TestIterBinarySearch(t *testing.T) {
 	for _, test := range searchTests {
-		actualValue, _ := BinaryIterative(test.data, test.key, 0, len(test.data)-1)
+		actualValue, actualError := BinaryIterative(test.data, test.key, 0, len(test.data)-1)
 		if actualValue != test.expected {
 			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
 		}
 	}
 }
--- search/testcases.go
+++ search/testcases.go
@@ -1,22 +1,30 @@
 package search
 
 type searchTest struct {
-	data     []int
-	key      int
-	expected int
-	name     string
+	data          []int
+	key           int
+	expected      int
+	expectedError error
+	name          string
 }
 
 // Note that these are immutable therefore they are shared among all the search tests.
 // If your algorithm is mutating these then it is advisable to create separate test cases.
 var searchTests = []searchTest{
 	//Sanity
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, "Sanity"},
-	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 9, 8, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 8, 7, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 7, 6, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 6, 5, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 4, 3, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3, 2, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, 1, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, nil, "Sanity"},
 	//Absent
-	{[]int{1, 4, 5, 6, 7, 10}, -25, -1, "Absent"},
-	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, "Absent"},
+	{[]int{1, 4, 5, 6, 7, 10}, -25, -1, ErrNotFound, "Absent"},
+	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, ErrNotFound, "Absent"},
 	//Empty slice
-	{[]int{}, 2, -1, "Empty"},
+	{[]int{}, 2, -1, ErrNotFound, "Empty"},
 }
