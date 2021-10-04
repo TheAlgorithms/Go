419f26d5fc507c318125bad249f006af469c3372
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-10-04 10:42:47 +0530

    exported the functions in math/straightlines

--- math/straightlines/straightlines.go
+++ math/straightlines/straightlines.go
@@ -14,13 +14,13 @@ type Line struct {
 }
 
 // Calculates the shortest distance between two points.
-func distance(a, b *Point) float64 {
+func Distance(a, b *Point) float64 {
 	return math.Sqrt(math.Pow(a.X-b.X, 2) + math.Pow(a.Y-b.Y, 2))
 }
 
 // Calcualtes the Point that divides a line in specific ratio.
 // DO NOT specify the ratio in the form m:n, specify it as r, where r = m / n.
-func section(p1, p2 *Point, r float64) Point {
+func Section(p1, p2 *Point, r float64) Point {
 	var point Point
 	point.X = (r*p2.X + p1.X) / (r + 1)
 	point.Y = (r*p2.Y + p1.Y) / (r + 1)
@@ -28,27 +28,27 @@ func section(p1, p2 *Point, r float64) Point {
 }
 
 // Calculates the slope (gradient) of a line.
-func slope(l *Line) float64 {
+func Slope(l *Line) float64 {
 	return (l.P2.Y - l.P1.Y) / (l.P2.X - l.P1.X)
 }
 
 // Calculates the Y-Intercept of a line from a specific Point.
-func intercept(p *Point, slope float64) float64 {
+func Intercept(p *Point, slope float64) float64 {
 	return p.Y - (slope * p.X)
 }
 
 // Checks if two lines are parallel or not.
-func isParallel(l1, l2 *Line) bool {
-	return slope(l1) == slope(l2)
+func IsParallel(l1, l2 *Line) bool {
+	return Slope(l1) == Slope(l2)
 }
 
 // Checks if two lines are perpendicular or not.
-func isPerpendicular(l1, l2 *Line) bool {
-	return slope(l1)*slope(l2) == -1
+func IsPerpendicular(l1, l2 *Line) bool {
+	return Slope(l1)*Slope(l2) == -1
 }
 
 // Calculates the distance of a given Point from a given line.
 // The slice should contain the coefficiet of x, the coefficient of y and the constant in the respective order.
-func pointDistance(p *Point, equation [3]float64) float64 {
+func PointDistance(p *Point, equation [3]float64) float64 {
 	return math.Abs(equation[0]*p.X+equation[1]*p.Y+equation[2]) / math.Sqrt(math.Pow(equation[0], 2)+math.Pow(equation[1], 2))
 }
--- math/straightlines/straightlines_test.go
+++ math/straightlines/straightlines_test.go
@@ -8,7 +8,7 @@ func TestDistance(t *testing.T) {
 	p1 := Point{0, 0}
 	p2 := Point{3, 4}
 	var wantedDistance float64 = 5
-	var calculatedDistance float64 = distance(&p1, &p2)
+	var calculatedDistance float64 = Distance(&p1, &p2)
 	if calculatedDistance != wantedDistance {
 		t.Fatalf("Failed to calculate Distance.")
 	}
@@ -18,7 +18,7 @@ func TestSection(t *testing.T) {
 	p1 := Point{1, 0}
 	p2 := Point{5, 0}
 	wantedPoint := Point{3, 0}
-	calculatedPoint := section(&p1, &p2, 1)
+	calculatedPoint := Section(&p1, &p2, 1)
 	if calculatedPoint != wantedPoint {
 		t.Fatalf("Failed to calculate Section.")
 	}
@@ -27,7 +27,7 @@ func TestSection(t *testing.T) {
 func TestSlope(t *testing.T) {
 	line := Line{P1: Point{1, 2}, P2: Point{2, 4}}
 	var wantedSlope float64 = 2
-	var calculatedSlope float64 = slope(&line)
+	var calculatedSlope float64 = Slope(&line)
 	if calculatedSlope != wantedSlope {
 		t.Fatalf("Failed to calculate Slope.")
 	}
@@ -37,7 +37,7 @@ func TestIntercept(t *testing.T) {
 	p := Point{0, 3}
 	var slope float64 = -5
 	var wantedIntercept float64 = 3
-	var calculatedIntercept float64 = intercept(&p, slope)
+	var calculatedIntercept float64 = Intercept(&p, slope)
 	if calculatedIntercept != wantedIntercept {
 		t.Fatalf("Failed to calculate Intercept.")
 	}
@@ -46,7 +46,7 @@ func TestIntercept(t *testing.T) {
 func TestIsParallel(t *testing.T) {
 	l1 := Line{P1: Point{1, 2}, P2: Point{2, 4}}
 	l2 := Line{P1: Point{25, 50}, P2: Point{50, 100}}
-	if !isParallel(&l1, &l2) {
+	if !IsParallel(&l1, &l2) {
 		t.Fatalf("Failed to check if Parallel.")
 	}
 }
@@ -54,7 +54,7 @@ func TestIsParallel(t *testing.T) {
 func TestIsPerpendicular(t *testing.T) {
 	l1 := Line{P1: Point{1, 2}, P2: Point{2, 4}}
 	l2 := Line{P1: Point{2, 2}, P2: Point{4, 1}}
-	if !isPerpendicular(&l1, &l2) {
+	if !IsPerpendicular(&l1, &l2) {
 		t.Fatalf("Failed to check if Perpendicular.")
 	}
 }
@@ -63,7 +63,7 @@ func TestPointDistance(t *testing.T) {
 	p := Point{1, 1}
 	equation := [3]float64{4, 3, 1}
 	var wantedDistance float64 = 1.6
-	var calculatedDistance float64 = pointDistance(&p, equation)
+	var calculatedDistance float64 = PointDistance(&p, equation)
 	if calculatedDistance != wantedDistance {
 		t.Fatalf("Failed to calculate Point Distance.")
 	}
