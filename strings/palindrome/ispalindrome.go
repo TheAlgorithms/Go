4cd0ddca33cc422d9d3d35fcf70da6f3cd7a5a66
fernandakawasaki <fernandakawasaki@hotmail.com>
2021-10-11 21:23:09 -0300

    feat: add ispalindrome algorithm

---
+++ strings/palindrome/ispalindrome.go
@@ -0,0 +1,25 @@
+package IsPalindrome
+
+import (
+	"regexp"
+	"strings"
+)
+
+func CleanString(text string) string {
+	clean_text := strings.ToLower(text)
+	clean_text = strings.Join(strings.Fields(clean_text), "") // Remove spaces
+	regex, _ := regexp.Compile("[^a-zA-Z0-9 ]+")              // Regular expression for alphanumeric only characters
+	return regex.ReplaceAllString(clean_text, "")
+}
+
+func IsPalindrome(text string) bool {
+	clean_text := CleanString(text)
+	var i, j int
+	for i = 0; i < len(clean_text)/2; i++ {
+		j = len(clean_text) - 1 - i
+		if clean_text[i] != clean_text[j] {
+			return false
+		}
+	}
+	return true
+}
