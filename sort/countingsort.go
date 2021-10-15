913f4ec1543532d988d72ec1f3ae24dc7cb44a84
Administrator <pi@pglp.noip.me>
2021-10-15 13:45:54 +0200

    add counting sort

---
+++ sort/countingsort.go
@@ -0,0 +1,26 @@
+// countingsort.go
+// description: Implementation of counting sort algorithm
+// details: A simple counting sort algorithm implementation
+// author [Phil](https://github.com/pschik)
+// see sort_test.go for a test implementation, test function TestQuickSort
+
+// package sort provides primitives for sorting slices and user-defined collections
+package sort
+
+func CountSort(data []int, maxValue int) []int {
+	bucketLen := maxValue + 1
+	bucket := make([]int, bucketLen)
+	sortedIndex := 0
+	length := len(data)
+	for i := 0; i < length; i++ {
+		bucket[data[i]] += 1
+	}
+	for j := 0; j < bucketLen; j++ {
+		for bucket[j] > 0 {
+			data[sortedIndex] = j
+			sortedIndex += 1
+			bucket[j] -= 1
+		}
+	}
+	return data
+}
--- sort/sorts_test.go
+++ sort/sorts_test.go
@@ -23,28 +23,24 @@ func testFramework(t *testing.T, sortingFunction func([]int) []int) {
 			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Reversed Unsigned",
 		},
-
 		//Sorted slice
 		{
 			input:    []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Sorted Signed",
 		},
-
 		//Reversed slice
 		{
 			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Reversed Signed",
 		},
-
 		//Reversed slice, even length
 		{
 			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Reversed Signed #2",
 		},
-
 		//Random order with repetitions
 		{
 			input:    []int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
@@ -75,6 +71,7 @@ func testFramework(t *testing.T, sortingFunction func([]int) []int) {
 	}
 }
 
+
 //BEGIN TESTS
 
 func TestBubble(t *testing.T) {
@@ -91,6 +88,54 @@ func TestMerge(t *testing.T) {
 
 func TestHeap(t *testing.T) {
 	testFramework(t, HeapSort)
+}
+
+func TestCount(t *testing.T) {
+	testCases := []struct {
+		input    []int
+		expected []int
+		name     string
+	}{
+		//Sorted slice
+		{
+			input:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Sorted Unsigned",
+		},
+		//Reversed slice
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Unsigned",
+		},
+		//Reversed slice, even length
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Signed #2",
+		},
+		//Random order with repetitions
+		{
+			input:    []int{7, 4, 6, 5, 8, 3, 2, 9, 10, 9, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10},
+			name:     "Random order Signed",
+		},
+		//Single-entry slice
+		{
+			input:    []int{1},
+			expected: []int{1},
+			name:     "Singleton",
+		},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			CountSort(test.input, max(test.input))
+			if !reflect.DeepEqual(test.input, test.expected) {
+				t.Errorf("test %s failed", test.name)
+				t.Errorf("actual %v expected %v", test.input, test.expected)
+			}
+		})
+	}
 }
 
 func TestQuick(t *testing.T) {
@@ -111,28 +156,24 @@ func TestQuick(t *testing.T) {
 			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Reversed Unsigned",
 		},
-
 		//Sorted slice
 		{
 			input:    []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Sorted Signed",
 		},
-
 		//Reversed slice
 		{
 			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Reversed Signed",
 		},
-
 		//Reversed slice, even length
 		{
 			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			name:     "Reversed Signed #2",
 		},
-
 		//Random order with repetitions
 		{
 			input:    []int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
@@ -151,7 +192,6 @@ func TestQuick(t *testing.T) {
 			name:     "Empty Slice",
 		},
 	}
-
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			QuickSort(test.input, 0, len(test.input)-1)
@@ -161,7 +201,6 @@ func TestQuick(t *testing.T) {
 			}
 		})
 	}
-
 }
 
 func TestShell(t *testing.T) {
@@ -172,16 +211,8 @@ func TestRadix(t *testing.T) {
 	testFramework(t, RadixSort)
 }
 
-// Very slow, consider commenting
-// func TestSelection(t *testing.T) {
-// 	testFramework(t, SelectionSort)
-// }
-
-/* func TestTopological(t *testing.T) {
-testFramework(t, topologicalSort)
-} */
-
 //END TESTS
+
 
 func benchmarkFramework(b *testing.B, f func(arr []int) []int) {
 	var sortTests = []struct {
@@ -195,23 +226,18 @@ func benchmarkFramework(b *testing.B, f func(arr []int) []int) {
 		//Reversed slice
 		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
 			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Unsigned"},
-
 		//Sorted slice
 		{[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Sorted Signed"},
-
 		//Reversed slice
 		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Signed"},
-
 		//Reversed slice, even length
 		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Signed #2"},
-
 		//Random order with repetitions
 		{[]int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10}, "Random order Signed"},
-
 		//Empty slice
 		{[]int{}, []int{}, "Empty"},
 		//Single-entry slice
@@ -224,6 +250,7 @@ func benchmarkFramework(b *testing.B, f func(arr []int) []int) {
 		}
 	}
 }
+
 func BenchmarkBubble(b *testing.B) {
 	benchmarkFramework(b, bubbleSort)
 }
@@ -240,6 +267,41 @@ func BenchmarkHeap(b *testing.B) {
 	benchmarkFramework(b, HeapSort)
 }
 
+func BenchmarkCount(b *testing.B) {
+	var sortTests = []struct {
+		input    []int
+		expected []int
+		name     string
+	}{
+		//Sorted slice
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Sorted Unsigned"},
+		//Reversed slice
+		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Unsigned"},
+		//Sorted slice
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Sorted Signed"},
+		//Reversed slice
+		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Signed"},
+		//Reversed slice, even length
+		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Signed #2"},
+		//Random order with repetitions
+		{[]int{7, 4, 6, 5, 8, 3, 2, 9, 10, 9, 1},
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10}, "Random order Signed"},
+		//Single-entry slice
+		{[]int{1}, []int{1}, "Singleton"},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, test := range sortTests {
+			CountSort(test.input, max(test.input))
+		}
+	}
+}
+
 func BenchmarkQuick(b *testing.B) {
 	var sortTests = []struct {
 		input    []int
@@ -252,23 +314,18 @@ func BenchmarkQuick(b *testing.B) {
 		//Reversed slice
 		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
 			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Unsigned"},
-
 		//Sorted slice
 		{[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Sorted Signed"},
-
 		//Reversed slice
 		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Signed"},
-
 		//Reversed slice, even length
 		{[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, "Reversed Signed #2"},
-
 		//Random order with repetitions
 		{[]int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
 			[]int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10}, "Random order Signed"},
-
 		//Empty slice
 		{[]int{}, []int{}, "Empty"},
 		//Single-entry slice
