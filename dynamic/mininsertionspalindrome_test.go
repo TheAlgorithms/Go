1d50607c212854ba13d8aad2d9d9aed073a30957
Likhith Praveen <likhith.praveen@siply.in>
2021-10-16 22:53:34 +0530

    ran gofmt -w in _test file

--- dynamic/mininsertionspalindrome_test.go
+++ dynamic/mininsertionspalindrome_test.go
@@ -5,26 +5,26 @@ import (
 	"testing"
 )
 
-func Test_FindMinInsertionsPalindrome(t *testing.T)  {
-	
+func Test_FindMinInsertionsPalindrome(t *testing.T) {
+
 	var testCases = []struct {
-		str string
+		str      string
 		expected int
 	}{
-		{"a",0},
-		{"ab",1},
-		{"abcd",3},
-		{"abcda",2},
-		{"abcde",4},
-		{"geeks",3},
-		{"thealgorithms",10},
+		{"a", 0},
+		{"ab", 1},
+		{"abcd", 3},
+		{"abcda", 2},
+		{"abcde", 4},
+		{"geeks", 3},
+		{"thealgorithms", 10},
 	}
 
 	for i := range testCases {
-		t.Run(fmt.Sprintf("string: %s",testCases[i].str), func(t *testing.T) {
+		t.Run(fmt.Sprintf("string: %s", testCases[i].str), func(t *testing.T) {
 			result := FindMinInsertionsPalindrome(testCases[i].str)
 			if result != testCases[i].expected {
-				t.Errorf("Expected %d insertions for %s, but found %d",testCases[i].expected,testCases[i].str,result)
+				t.Errorf("Expected %d insertions for %s, but found %d", testCases[i].expected, testCases[i].str, result)
 			}
 		})
 	}
