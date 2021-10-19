bd2df217d089ea120eb5a961940c005d53add3ae
Motasim <makkimotasim@gmail.com>
2021-10-19 10:45:56 +0530

    feat: Binary to Decimal Conversion

---
+++ conversion/binarytodecimal.go
@@ -0,0 +1,41 @@
+/*
+Author: Motasim
+GitHub: https://github.com/motasimmakki
+Date: 19-Oct-2021
+*/
+
+// This algorithm will convert any Binary number(0 or 1) to Decimal number(+ve number).
+// https://en.wikipedia.org/wiki/Binary_number
+// https://en.wikipedia.org/wiki/Decimal
+// Function receives a Binary Number as string and returns the Decimal number as integer.
+// Suppoted Binary number range is 0 to 2^(31-1).
+
+// Package name.
+package conversion
+
+// Importing necessary package.
+import (
+	"errors"
+	"regexp"
+)
+
+var IsValid = regexp.MustCompile("^[0-1]{1,}$").MatchString
+
+// BinaryToDecimal() function that will take Binary number as string,
+// and return it's Decimal equivalent as integer.
+func BinaryToDecimal(num string) (int, error) {
+	if !IsValid(num) {
+		return -1, errors.New("not a valid binary string")
+	}
+	if len(num) > 32 {
+		return -1, errors.New("binary number must be in range 0 to 2^(31-1)")
+	}
+	var result, base int = 0, 1
+	for i := len(num) - 1; i >= 0; i-- {
+		if num[i] == '1' {
+			result += base
+		}
+		base *= 2
+	}
+	return result, nil
+}
