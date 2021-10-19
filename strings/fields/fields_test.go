1459e15c3eba785142055e9c764a3cda42ba1607
Anony <73958752+anonyindian@users.noreply.github.com>
2021-10-19 11:30:45 +0530

    change function name

--- strings/fields/fields_test.go
+++ strings/fields/fields_test.go
@@ -23,7 +23,7 @@ func TestNeoSplit(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := FieldsN(tt.args.s, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+			if got := NeoSplit(tt.args.s, tt.args.n); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("FieldsN() = %v, want %v", got, tt.want)
 			}
 		})
