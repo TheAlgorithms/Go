01378e8c89c178b5377aaf3b19d468fc2c893f45
calebeof <calebeof14@gmail.com>
2021-10-21 12:05:03 -0300

    adjust min and max

--- structure/segmenttree/segmenttree.go
+++ structure/segmenttree/segmenttree.go
@@ -7,7 +7,8 @@
 package segmenttree
 
 import (
-	"github.com/TheAlgorithms/Go/dynamic"
+	"github.com/TheAlgorithms/Go/math/max"
+	"github.com/TheAlgorithms/Go/math/min"
 )
 
 const emptyLazyNode = -1
@@ -59,8 +60,8 @@ func (s *SegmentTree) Query(node int, leftNode int, rightNode int, firstIndex in
 	//get sum of left and right nodes
 	mid := (leftNode + rightNode) / 2
 
-	leftNodeSum := s.Query(2*node, leftNode, mid, firstIndex, dynamic.Min(mid, lastIndex))
-	rightNodeSum := s.Query(2*node+1, mid+1, rightNode, dynamic.Max(firstIndex, mid+1), lastIndex)
+	leftNodeSum := s.Query(2*node, leftNode, mid, firstIndex, min.Int(mid, lastIndex))
+	rightNodeSum := s.Query(2*node+1, mid+1, rightNode, max.Int(firstIndex, mid+1), lastIndex)
 
 	return leftNodeSum + rightNodeSum
 }
@@ -86,8 +87,8 @@ func (s *SegmentTree) Update(node int, leftNode int, rightNode int, firstIndex i
 		//update left and right nodes
 		mid := (leftNode + rightNode) / 2
 
-		s.Update(2*node, leftNode, mid, firstIndex, dynamic.Min(mid, lastIndex), value)
-		s.Update(2*node+1, mid+1, rightNode, dynamic.Max(firstIndex, mid+1), lastIndex, value)
+		s.Update(2*node, leftNode, mid, firstIndex, min.Int(mid, lastIndex), value)
+		s.Update(2*node+1, mid+1, rightNode, max.Int(firstIndex, mid+1), lastIndex, value)
 
 		s.SegmentTree[node] = s.SegmentTree[2*node] + s.SegmentTree[2*node+1]
 	}
