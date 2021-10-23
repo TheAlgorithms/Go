004cc9d940c1724b8f19c1efa941deb808123f07
Fernanda Kawasaki <50497814+fernandakawasaki@users.noreply.github.com>
2021-10-23 05:04:41 -0300

    merge: feat: add palindrome checker (#386)
    * feat: add ispalindrome algorithm
    
    * test: add test for ispalindrome algorithm
    
    * fix: change package name to respect naming conventions
    
    * fix: add description and adapt to utf8
    
    * Update strings/palindrome/ispalindrome.go
    
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>
    
    * fix: Update strings/palindrome/ispalindrome.go
    
    Co-authored-by: Rak Laptudirm <raklaptudirm@gmail.com>
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>

---
+++ strings/palindrome/ispalindrome_test.go
@@ -0,0 +1,53 @@
+package palindrome
+
+import (
+	"testing"
+)
+
+var testCases = []struct {
+	name     string // test description
+	input    string // user input
+	expected bool   // expected return
+}{
+	{
+		"non palindrome string",
+		"According to the laws of aviation bees can't flyã",
+		false,
+	},
+	{
+		"non palindrome string",
+		"Alô?",
+		false,
+	},
+	{
+		"palindrome string 1",
+		"Do geese see God?",
+		true,
+	},
+	{
+		"palindrome string 2",
+		"ΝΙΨΟΝ ΑΝΟΜΗΜΑΤΑ ΜΗ ΜΟΝΑΝ ΟΨΙΝ",
+		true,
+	},
+	{
+		"palindrome string 3",
+		"Was it a car or a cat I saw?",
+		true,
+	},
+	{
+		"empty string",
+		"",
+		true,
+	},
+}
+
+func TestIsPalindrome(t *testing.T) {
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			func_result := IsPalindrome(test.input)
+			if test.expected != func_result {
+				t.Errorf("Expected answer '%t' for string '%s' but answer given was %t", test.expected, test.input, func_result)
+			}
+		})
+	}
+}
