db791478f0889fd33a7bdc09cb1ef363d3ae39e1
Meet Valani <40715819+meetvalani@users.noreply.github.com>
2021-10-24 10:55:52 +0530

    Update math/max/max.go
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>

--- math/max/max.go
+++ math/max/max.go
@@ -1,6 +1,6 @@
 package max
 
-// Int is a function which returns the maximum of all the integers.
+// Int is a function which returns the maximum of all the integers provided as arguments.
 func Int(values ...int) int {
 	max := values[0]
 	for _, value := range values {
