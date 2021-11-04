9f125a85967e12eb0a2fd69c2699e93f581a17c1
Taj <tjgurwara99@users.noreply.github.com>
2021-11-04 18:56:32 +0000

    fix: incorrect documentation format

--- structure/set/setexample_test.go
+++ structure/set/setexample_test.go
@@ -1,7 +1,7 @@
 // package set implements a Set using a golang map.
 // This implies that only the types that are accepted as valid map keys can be used as set elements.
 // For instance, do not try to Add a slice, or the program will panic.
-//
+
 package set
 
 import (
