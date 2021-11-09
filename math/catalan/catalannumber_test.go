3408a79b28c47945265cbf7fa171b6dd31c7c820
Ilya Sokolov <developer.sokolov@gmail.com>
2021-11-09 23:54:02 +0300

    fix: change test struct

--- math/catalan/catalannumber_test.go
+++ math/catalan/catalannumber_test.go
@@ -8,25 +8,22 @@ package catalan
 import "testing"
 
 func TestCatalanNumber(t *testing.T) {
-	type args struct {
-		n int
-	}
 	tests := []struct {
 		name string
-		args args
+		n    int
 		want int
 	}{
-		{"zero Catalan number ", args{0}, 1},
-		{"second Catalan number ", args{2}, 2},
-		{"third Catalan number ", args{3}, 5},
-		{"fourth Catalan number ", args{4}, 14},
-		{"fifth Catalan number ", args{5}, 42},
-		{"sixth Catalan number ", args{6}, 132},
-		{"tenth Catalan number ", args{10}, 16796},
+		{"zero Catalan number ", 0, 1},
+		{"second Catalan number ", 2, 2},
+		{"third Catalan number ", 3, 5},
+		{"fourth Catalan number ", 4, 14},
+		{"fifth Catalan number ", 5, 42},
+		{"sixth Catalan number ", 6, 132},
+		{"tenth Catalan number ", 10, 16796},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			if got := CatalanNumber(test.args.n); got != test.want {
+			if got := CatalanNumber(test.n); got != test.want {
 				t.Errorf("CatalanNumber() = %v, want %v", got, test.want)
 			}
 		})
