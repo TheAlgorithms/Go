c87d6d6593a90a37c9a2696ee16bda062c12d149
Paul Leydier <paul.leydier@compass-ft.com>
2021-11-24 00:16:39 +0100

    feat: Parallel MonteCarloPi using channels

--- math/pi/montecarlopi.go
+++ math/pi/montecarlopi.go
@@ -8,7 +8,9 @@
 package pi
 
 import (
+	"fmt"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -24,4 +26,54 @@ func MonteCarloPi(randomPoints int) float64 {
 	}
 	pi := float64(inside) / float64(randomPoints) * 4
 	return pi
+}
+
+func MonteCarloPi2(n int) (float64, error) {
+	numCPU := runtime.GOMAXPROCS(0)
+	c := make(chan int, numCPU)
+	pointsToDraw, err := splitInt(n, numCPU)
+	if err != nil {
+		return 0, err
+	}
+	for _, p := range pointsToDraw {
+		go drawPoints(p, c)
+	}
+	inside := 0
+	for i := 0; i < numCPU; i++ {
+		inside += <-c
+	}
+	return float64(inside) / float64(n) * 4, nil
+}
+
+func drawPoints(n int, c chan<- int) {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	inside := 0
+	for i := 0; i < n; i++ {
+		x, y := rnd.Float64(), rnd.Float64()
+		if x*x+y*y <= 1 {
+			inside++
+		}
+	}
+	c <- inside
+}
+
+func splitInt(x int, n int) ([]int, error) {
+	if x < n {
+		return nil, fmt.Errorf("x must be < n - given values are x=%d, n=%d", x, n)
+	}
+	split := make([]int, n)
+	if x%n == 0 {
+		for i := 0; i < n; i++ {
+			split[i] = x / n
+		}
+	} else {
+		limit := x % n
+		for i := 0; i < limit; i++ {
+			split[i] = x/n + 1
+		}
+		for i := limit; i < n; i++ {
+			split[i] = x / n
+		}
+	}
+	return split, nil
 }
