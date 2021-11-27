c85f9314b5c8b2c0ae4fad3c39de326dbf322d97
Paul Leydier <75126792+paul-leydier@users.noreply.github.com>
2021-11-27 23:01:25 +0100

    feat: add math/MontecarloPi2 implementation (#448)
    * feat: Parallel MonteCarloPi using channels
    
    * test: Parallel MonteCarloPi using channels
    
    * doc: MonteCarloPi2 documentation
    
    * doc: fix typo
    
    * feat: rename MonteCarloPi2 to MonteCarloPiConcurrent
    
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>

--- math/pi/montecarlopi_test.go
+++ math/pi/montecarlopi_test.go
@@ -25,3 +25,59 @@ func BenchmarkMonteCarloPi(b *testing.B) {
 		MonteCarloPi(100000000)
 	}
 }
+
+func TestSplitInt(t *testing.T) {
+	testCases := []struct {
+		name           string
+		x              int
+		n              int
+		expectedResult []int
+		expectedError  bool
+	}{
+		{"multiple", 10, 5, []int{2, 2, 2, 2, 2}, false},
+		{"n=1", 10, 1, []int{10}, false},
+		{"x=10, n=3", 10, 3, []int{4, 3, 3}, false},
+		{"x=10, n=7", 10, 7, []int{2, 2, 2, 1, 1, 1, 1}, false},
+		{"n>x", 10, 11, nil, true},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			res, err := splitInt(testCase.x, testCase.n)
+			for i := 0; i < len(testCase.expectedResult); i++ {
+				if res[i] != testCase.expectedResult[i] {
+					t.Fatalf("unexpected result at index %d: %d - expected %d", i, res[i], testCase.expectedResult[i])
+				}
+			}
+			if testCase.expectedError {
+				if err == nil {
+					t.Fatal("expected an error, got nil")
+				}
+			} else {
+				if err != nil {
+					t.Fatalf("unexpected error - %s", err)
+				}
+			}
+		})
+	}
+}
+
+func TestMonteCarloPi2(t *testing.T) {
+	res, err := MonteCarloPiConcurrent(100000000)
+	if err != nil {
+		t.Errorf("unexpected error %s", err)
+	}
+	result := fmt.Sprintf("%.2f", res)
+	t.Log(result)
+	if result != "3.14" {
+		t.Errorf("Wrong result! Expected:%f, returned:%s ", 3.1415, result)
+	}
+}
+
+func BenchmarkMonteCarloPi2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := MonteCarloPiConcurrent(100000000)
+		if err != nil {
+			b.Fatalf("unexpected error - %s", err)
+		}
+	}
+}
