10b50f63381c477bc3509cfced8c45aad975b494
Aniruddha <aniruddha97bhatt@gmail.com>
2021-12-06 10:01:26 +0530

    Inital commit for articulation point test

--- graph/articulation_point_test.go
+++ graph/articulation_point_test.go
@@ -1,10 +1,70 @@
+package graph
+
 import (
+	"reflect"
 	"testing"
 )
 
 func TestArticulationPoints(t *testing.T) {
 	var testCases = []struct {
-		name string
-		graph Graph
+		description string
+		graph       Graph
+		expected    []bool
+	}{
+		{
+			"Linear tree structure",
+			Graph{
+				vertices: 5,
+				edges: map[int]map[int]int{
+					0: map[int]int{
+						1: 0,
+					},
+
+					1: map[int]int{
+						2: 0,
+					},
+
+					2: map[int]int{
+						3: 0,
+					},
+
+					3: map[int]int{
+						4: 0,
+					},
+				},
+			},
+			[]bool{false, true, true, true, false},
+		}, {
+			"A complete graph",
+			Graph{
+				vertices: 4,
+				edges: map[int]map[int]int{
+					0: map[int]int{
+						1: 0,
+						2: 0,
+						3: 0,
+					},
+
+					1: map[int]int{
+						2: 0,
+						3: 0,
+					},
+
+					2: map[int]int{
+						3: 0,
+					},
+				},
+			},
+			[]bool{false, false, false, false},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.description, func(t *testing.T) {
+			is_ap := ArticulationPoint(test.graph)
+			if reflect.DeepEqual(is_ap, test.expected) {
+				t.Logf("FAIL: %s", test.description)
+			}
+		})
 	}
 }
