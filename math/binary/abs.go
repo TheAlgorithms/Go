8522e6cd50cf42f69efe8d78e88afeb61902bd19
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-21 12:50:28 +0300

    fix: add comment

--- math/binary/abs.go
+++ math/binary/abs.go
@@ -1,6 +1,11 @@
 package binary
 
 // ABS - this function return absolute value use binary operation
+// Principle of operation:
+// 1) Get the mask by right shift by the bit width of the variable (int64, int32, int16, int8)
+// 2) For negative numbers, above step sets mask as 1 1 1 1 1 1 1 1 and 0 0 0 0 0 0 0 0 for positive numbers.
+// 3) Add the mask to the given number.
+// 4) XOR of mask + n and mask gives the absolute value.
 func ABS(base, n int) int {
 	m := n >> (base - 1)
 	return (n + m) ^ m
