02a71d5c7f41c1bdce141d2fe8d0bd12140d07e9
Aniruddha <aniruddha97bhatt@gmail.com>
2021-12-27 11:02:35 +0530

    Linting changes

--- graph/articulationpoints.go
+++ graph/articulationpoints.go
@@ -26,7 +26,13 @@ func ArticulationPoint(graph Graph) []bool {
 		discovery_time:     make([]int, graph.vertices), // integer slice to store the discovery time of a vertex as we traverse the graph in a depth first manner
 		earliest_discovery: make([]int, graph.vertices), // integer slice to store the earliest discovered vertex reachable from a vertex
 	}
-	articulationPointHelper(apHelperInstance, 0, -1, &time, graph)
+	articulationPointHelper(
+		apHelperInstance,
+		0,
+		-1,
+		&time,
+		graph,
+	)
 
 	if apHelperInstance.child_cnt[0] == 1 {
 		apHelperInstance.is_ap[0] = false // if the root has only one child, it is not an articulation point
@@ -38,12 +44,21 @@ func ArticulationPoint(graph Graph) []bool {
 // articulationPointHelper is a recursive function to traverse the graph and mark articulation points.
 // Based on the depth first search transversal of the graph, however modified to keep track
 // and update the child_cnt, discovery_time and earliest_discovery slices defined above
-func articulationPointHelper(apHelperInstance *apHelper, vertex int, parent int, time *int, graph Graph) {
+func articulationPointHelper(
+	apHelperInstance *apHelper,
+	vertex int,
+	parent int,
+	time *int,
+	graph Graph,
+) {
 	apHelperInstance.visited[vertex] = true
 
-	apHelperInstance.discovery_time[vertex] = *time                                       // Mark the time of discovery of a vertex
-	apHelperInstance.earliest_discovery[vertex] = apHelperInstance.discovery_time[vertex] // set the earliest discovery time to the discovered time
-	*time++                                                                               // increment the time
+	// Mark the time of discovery of a vertex
+	// set the earliest discovery time to the discovered time
+	// increment the time
+	apHelperInstance.discovery_time[vertex] = *time
+	apHelperInstance.earliest_discovery[vertex] = apHelperInstance.discovery_time[vertex]
+	*time++
 
 	for next_vertex := range graph.edges[vertex] {
 		if next_vertex == parent {
@@ -51,11 +66,23 @@ func articulationPointHelper(apHelperInstance *apHelper, vertex int, parent int,
 		}
 
 		if apHelperInstance.visited[next_vertex] { // if the vertex is already visited, then it is a back edge and manipulate earliest discovery time accordingly
-			apHelperInstance.earliest_discovery[vertex] = min.Int(apHelperInstance.earliest_discovery[vertex], apHelperInstance.discovery_time[next_vertex])
+			apHelperInstance.earliest_discovery[vertex] = min.Int(
+				apHelperInstance.earliest_discovery[vertex],
+				apHelperInstance.discovery_time[next_vertex],
+			)
 		} else {
 			apHelperInstance.child_cnt[vertex]++
-			articulationPointHelper(apHelperInstance, next_vertex, vertex, time, graph)
-			apHelperInstance.earliest_discovery[vertex] = min.Int(apHelperInstance.earliest_discovery[vertex], apHelperInstance.earliest_discovery[next_vertex])
+			articulationPointHelper(
+				apHelperInstance,
+				next_vertex,
+				vertex,
+				time,
+				graph,
+			)
+			apHelperInstance.earliest_discovery[vertex] = min.Int(
+				apHelperInstance.earliest_discovery[vertex],
+				apHelperInstance.earliest_discovery[next_vertex],
+			)
 			if apHelperInstance.earliest_discovery[next_vertex] >= apHelperInstance.discovery_time[vertex] {
 				apHelperInstance.is_ap[vertex] = true
 			}
