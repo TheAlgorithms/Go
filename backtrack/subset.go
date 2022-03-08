21be1873299a98c96f63aebc137f39881c78a779
sixway <lw.2675@qq.com>
2022-03-08 21:58:40 +0800

    fix: backtracking algorithm for subset - make algorithm not generate duplicate subset - add more test cases, use Table-driven test
    Signed-off-by: sixway <lw.2675@qq.com>

--- backtrack/subset.go
+++ backtrack/subset.go
@@ -1,38 +1,48 @@
 // subset.go
 // description: subset of a given set
 // details:
-//In mathematics, set A is a subset of a set B if all elements of A are also elements of B; B is then a superset of A. It is possible for A and B to be equal; if they are unequal, then A is a proper subset of B. The relationship of one set being a subset of another is called inclusion (or sometimes containment). A is a subset of B may also be expressed as B includes (or contains) A or A is included (or contained) in B.
-//  - https://en.wikipedia.org/wiki/Subset
+// In mathematics,
+// set A is a subset of a set B if all elements of A are also elements of B;
+// B is then a superset of A. It is possible for A and B to be equal;
+// if they are unequal, then A is a proper subset of B.
+// The relationship of one set being a subset of another
+// is called inclusion (or sometimes containment).
+// A is a subset of B may also be expressed as B includes (or contains) A
+// or A is included (or contained) in B.
+// - https://en.wikipedia.org/wiki/Subset
 // author: [sixwaaaay](https://github.com/sixwaaaay)
 // see subset_test.go
 
 package backtrack
 
-func Subset(nums []int) (set [][]int) {
+import "sort"
+
+// Subset is a function to generate all subsets of a given array
+func Subset(nums []int) [][]int {
+	sort.Ints(nums)
+	var subsets [][]int
 	n := len(nums)
-	// init index array for record the index of each element selected
 	index := make([]bool, n)
-	for i := 0; i < n; i++ {
-		index[i] = false
-	}
-	var backtrack func(int)
-	backtrack = func(depth int) {
-		// use index to record the selected elements
-		if depth == n {
-			temp := []int{}
-			for i := range index {
+	var backtrackSubset func(int)
+	backtrackSubset = func(depth int) {
+		if depth == n { // use index to record the selected elements
+			var subset []int
+			for i := 0; i < n; i++ {
 				if index[i] {
-					temp = append(temp, nums[i])
+					subset = append(subset, nums[i])
 				}
-			}
-			set = append(set, temp)
+			} // add index mapped array to subsets
+			subsets = append(subsets, subset)
+			return
+		}
+		backtrackSubset(depth + 1) // not select element at current depth
+		if depth > 0 && !index[depth-1] && nums[depth-1] == nums[depth] {
 			return
 		}
 		index[depth] = true
-		backtrack(depth + 1)
+		backtrackSubset(depth + 1) // select element at current depth
 		index[depth] = false
-		backtrack(depth + 1)
 	}
-	backtrack(0)
-	return
+	backtrackSubset(0)
+	return subsets
 }
--- backtrack/subset_test.go
+++ backtrack/subset_test.go
@@ -10,13 +10,78 @@ import (
 	"testing"
 )
 
-// test Subset function
+var testCasesForSubset = []struct {
+	name    string  // test case name
+	nums    []int   // nums array
+	subsets [][]int // expected subsets
+}{
+	{
+		name: "array without duplicate elements",
+		nums: []int{
+			2, 3, 4,
+		},
+		subsets: [][]int{
+			nil,
+			{4},
+			{3},
+			{3, 4},
+			{2},
+			{2, 4},
+			{2, 3},
+			{2, 3, 4},
+		},
+	},
+	{name: "array with two duplicate numbers",
+		nums: []int{
+			1, 3, 3,
+		},
+		subsets: [][]int{
+			nil,
+			{3},
+			{3, 3},
+			{1},
+			{1, 3},
+			{1, 3, 3},
+		},
+	},
+	{
+		name: "array with three repeating numbers",
+		nums: []int{0, 1, 0, 9, 0},
+		subsets: [][]int{
+			nil, {9}, {1}, {1, 9}, {0}, {0, 9}, {0, 1}, {0, 1, 9},
+			{0, 0}, {0, 0, 9}, {0, 0, 1}, {0, 0, 1, 9}, {0, 0, 0},
+			{0, 0, 0, 9}, {0, 0, 0, 1}, {0, 0, 0, 1, 9},
+		},
+	},
+	{
+		name: "empty array",
+		nums: nil,
+		subsets: [][]int{
+			nil,
+		},
+	},
+	{
+		name: "array with element greater than length",
+		nums: []int{7, 12},
+		subsets: [][]int{
+			nil,
+			{12},
+			{7},
+			{7, 12},
+		},
+	},
+}
+
 func TestSubset(t *testing.T) {
-	t.Run("generate subset for given array", func(t *testing.T) {
-		excepted := [][]int{{1, 2, 3}, {1, 2}, {1, 3}, {1}, {2, 3}, {2}, {3}, {}}
-		array := []int{1, 2, 3}
-		if reflect.DeepEqual(excepted, Subset(array)) == false {
-			t.Errorf("Wrong result! Expected:%v, returned:%v ", excepted, Subset(array))
-		}
-	})
+	for _, testCase := range testCasesForSubset {
+		t.Run(testCase.name, func(t *testing.T) {
+			subsets := Subset(testCase.nums)
+			if len(subsets) != len(testCase.subsets) {
+				t.Errorf("expected length %d, got %d", len(testCase.subsets), len(subsets))
+			}
+			if reflect.DeepEqual(subsets, testCase.subsets) == false {
+				t.Errorf("expected %v, got %v", testCase.subsets, subsets)
+			}
+		})
+	}
 }
