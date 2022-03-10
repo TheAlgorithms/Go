0bd2678199616efbb02aa4538338570950e4c88b
sixway <lw.2675@qq.com>
2022-03-10 23:16:03 +0800

    fix: replace closure with unexported package function
    Signed-off-by: sixway <lw.2675@qq.com>

--- backtrack/permutation.go
+++ backtrack/permutation.go
@@ -20,22 +20,22 @@ func Permutation(nums []int) [][]int {
 	perm := make([]int, n)
 	vis := make([]bool, n)
 	var permutations [][]int
-	var backtrackPerm func(int)
-	backtrackPerm = func(depth int) {
-		if depth == n {
-			permutations = append(permutations, append([]int{}, perm...))
-			return
-		}
-		for i := range nums {
-			if vis[i] || (i > 0 && !vis[i-1] && nums[i] == nums[i-1]) {
-				continue
-			} // get first element that never used/visited
-			perm[depth] = nums[i]
-			vis[i] = true
-			backtrackPerm(depth + 1)
-			vis[i] = false
-		}
-	}
-	backtrackPerm(0)
+	backtrackPerm(&permutations, nums, 0, n, perm, vis)
 	return permutations
+}
+
+func backtrackPerm(permutations *[][]int, nums []int, depth int, n int, perm []int, vis []bool) {
+	if depth == n {
+		*permutations = append(*permutations, append([]int{}, perm...))
+		return
+	}
+	for i := range nums {
+		if vis[i] || (i > 0 && !vis[i-1] && nums[i] == nums[i-1]) {
+			continue
+		} // get first element that never used/visited
+		perm[depth] = nums[i]
+		vis[i] = true
+		backtrackPerm(permutations, nums, depth+1, n, perm, vis)
+		vis[i] = false
+	}
 }
