c2e47ecddfd5ea838e23eb22d5206a4424d0ac37
zhuangyongxin <zhuangyongxin@qutoutiao.net>
2022-03-23 19:29:53 +0800

    fix:change /2 to >>1 for better performance

--- search/binary.go
+++ search/binary.go
@@ -7,7 +7,7 @@ func Binary(array []int, target int, lowIndex int, highIndex int) (int, error) {
 	if highIndex < lowIndex || len(array) == 0 {
 		return -1, ErrNotFound
 	}
-	mid := int(lowIndex + (highIndex-lowIndex)/2)
+	mid := int(lowIndex + (highIndex-lowIndex)>>1)
 	if array[mid] > target {
 		return Binary(array, target, lowIndex, mid-1)
 	} else if array[mid] < target {
@@ -25,7 +25,7 @@ func BinaryIterative(array []int, target int, lowIndex int, highIndex int) (int,
 	endIndex := highIndex
 	var mid int
 	for startIndex <= endIndex {
-		mid = int(startIndex + (endIndex-startIndex)/2)
+		mid = int(startIndex + (endIndex-startIndex)>>1)
 		if array[mid] > target {
 			endIndex = mid - 1
 		} else if array[mid] < target {
