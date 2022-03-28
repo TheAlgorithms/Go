4e60feb3ab1eb970933431710fb861869e07f61e
Rak Laptudirm <raklaptudirm@gmail.com>
2022-03-28 10:44:33 +0530

    chore: separate tests for the two versions

--- math/prime/millerrabinprimalitytest_test.go
+++ math/prime/millerrabinprimalitytest_test.go
@@ -2,20 +2,21 @@ package prime
 
 import "testing"
 
-func TestMillerRabinTest(t *testing.T) {
-	var tests = []struct {
-		name     string
-		input    int64
-		expected bool
-		rounds   int64
-		err      error
-	}{
-		{"smallest prime", 2, true, 5, nil},
-		{"random prime", 3, true, 5, nil},
-		{"neither prime nor composite", 1, false, 5, nil},
-		{"random non-prime", 10, false, 5, nil},
-		{"another random prime", 23, true, 5, nil},
-	}
+var tests = []struct {
+	name     string
+	input    int64
+	expected bool
+	rounds   int64
+	err      error
+}{
+	{"smallest prime", 2, true, 5, nil},
+	{"random prime", 3, true, 5, nil},
+	{"neither prime nor composite", 1, false, 5, nil},
+	{"random non-prime", 10, false, 5, nil},
+	{"another random prime", 23, true, 5, nil},
+}
+
+func TestMillerRabinProbabilistic(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			output, err := MillerRabinProbabilistic(test.input, test.rounds)
@@ -26,24 +27,30 @@ func TestMillerRabinTest(t *testing.T) {
 				t.Errorf("For input: %d, expected: %v", test.input, output)
 			}
 		})
+	}
+}
 
+func TestMillerRabinDeterministic(t *testing.T) {
+	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			output, err := MillerRabinDeterministic(test.input)
 			if err != test.err {
 				t.Errorf("For input: %d, unexpected error: %v, expected error: %v", test.input, err, test.err)
 			}
 			if output != test.expected {
-				t.Errorf("For input: %d, expected: %v", test.input, output)
+				t.Errorf("For input: %d, expected %v", test.input, output)
 			}
 		})
 	}
 }
 
-func BenchmarkMillerRabinPrimalityTest(b *testing.B) {
+func BenchmarkMillerRabinProbabilistic(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, _ = MillerRabinProbabilistic(23, 5)
 	}
+}
 
+func BenchmarkMillerRabinDeterministic(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, _ = MillerRabinDeterministic(23)
 	}
