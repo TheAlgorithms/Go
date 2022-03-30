bd8e6d52ba9f340761b6dc061fe43a5e5b7bbfd6
Taj <tjgurwara99@users.noreply.github.com>
2022-03-30 18:38:44 +0100

    Fix: restructure checksum package (#485)
    * fix: [generic + sort] All ready for generics
    
    * GoDocMD support for Go 1.18
    
    * Bump Go version in workflow
    
    * Updated Documentation in README.md
    
    * bump go version in GoLangCI lint action
    
    * fix: [generic + sort] review feedback
    
    * Updated Documentation in README.md
    
    * empty commit: stuck github action
    
    * fix: restructuring checksum package
    
    * Updated Documentation in README.md
    
    Co-authored-by: github-action <${GITHUB_ACTOR}@users.noreply.github.com>

--- checksum/luhn_test.go
+++ checksum/luhn_test.go
@@ -3,27 +3,31 @@
 // author(s) [red_byte](https://github.com/i-redbyte)
 // see luhn.go
 
-package checksum
+package checksum_test
 
-import "testing"
+import (
+	"testing"
 
-func TestLuhnAlgorithm(t *testing.T) {
+	"github.com/TheAlgorithms/Go/checksum"
+)
+
+func TestLuhn(t *testing.T) {
 	tests := []struct {
 		name string
-		s    []rune
+		s    []byte
 		want bool
 	}{
-		{"check 4242424242424242", []rune("4242424242424242"), true},
-		{"check 6200000000000005", []rune("6200000000000005"), true},
-		{"check 5534200028533164", []rune("5534200028533164"), true},
-		{"check 36227206271667", []rune("36227206271667"), true},
-		{"check 471629309440", []rune("471629309440"), false},
-		{"check 1111", []rune("1111"), false},
-		{"check 12345674", []rune("12345674"), true},
+		{"check 4242424242424242", []byte("4242424242424242"), true},
+		{"check 6200000000000005", []byte("6200000000000005"), true},
+		{"check 5534200028533164", []byte("5534200028533164"), true},
+		{"check 36227206271667", []byte("36227206271667"), true},
+		{"check 471629309440", []byte("471629309440"), false},
+		{"check 1111", []byte("1111"), false},
+		{"check 12345674", []byte("12345674"), true},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			if got := LuhnAlgorithm(test.s); got != test.want {
+			if got := checksum.Luhn(test.s); got != test.want {
 				t.Errorf("LuhnAlgorithm() = %v, want %v", got, test.want)
 			}
 		})
@@ -32,6 +36,6 @@ func TestLuhnAlgorithm(t *testing.T) {
 
 func BenchmarkBruteForceFactorial(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		LuhnAlgorithm([]rune("4242424242424242"))
+		checksum.Luhn([]byte("4242424242424242"))
 	}
 }
