e4611128e04d69a822cbf7d5891226846b036476
nursyah21 <64634372+nursyah21@users.noreply.github.com>
2022-04-16 22:10:15 +0700

    added description CoinChange

--- dynamic/coinchange.go
+++ dynamic/coinchange.go
@@ -1,14 +1,11 @@
 package dynamic
 
-// return combination of coin
+// CoinChange 
+// function to find combination coins to get amount
+// return a number of combination coins
 func CoinChange(coins []int32, amount int32) int32 {
 	combination := make([]int32, amount)
 	combination[0] = 1
-
-	//Time Complexity O(mn)
-	//Space Complexity O(n)
-	//m is length coins
-	//n is amount
 
 	for _, c := range coins { // iterate combination of coin
 		for i := c; i < amount; i++ {
