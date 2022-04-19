a31236bd1972b9dc9d053ef3cf12b50b039699e8
nursyahjay <nursyahjaya@gmail.com>
2022-04-19 12:15:19 +0700

    fix  documentation coinchange.go

--- dynamic/coinchange.go
+++ dynamic/coinchange.go
@@ -1,13 +1,13 @@
 package dynamic
 
-// CoinChange 
-// function to find combination coins to get amount
-// return a number of combination coins
+// CoinChange finds the number of possible combinations of coins
+// of different values which can get to the target amount.
 func CoinChange(coins []int32, amount int32) int32 {
 	combination := make([]int32, amount)
 	combination[0] = 1
 
-	for _, c := range coins { // iterate combination of coin
+	// iterate combination of coin
+	for _, c := range coins {
 		for i := c; i < amount; i++ {
 			// find previous value
 			combination[i] += combination[i-c]
