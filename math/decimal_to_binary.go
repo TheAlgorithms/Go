9bd8b1cb8a01fe544f7f692a96265b0353b591cb
MIDHUN <84015942+MidhunManu@users.noreply.github.com>
2022-07-04 14:57:11 +0530

    Update decimal_to_binary.go

--- math/decimal_to_binary.go
+++ math/decimal_to_binary.go
@@ -2,19 +2,10 @@ package math
 import "fmt"
 
 func decimal_to_binary(num int) {
-	var binary []int
-
-   for num !=0 {
-      binary = append(binary, num%2)
-      num = num / 2
-   }
-   if len(binary)==0{
-      fmt.Printf("%d\n", 0)
-   } else {
-      for i:=len(binary)-1; i>=0; i--{
-         fmt.Printf("%d", binary[i])
-      }
-      fmt.Println()
-   }
+	if num >= 1 {
+		decimal_to_binary(num / 2)
+	}
+	fmt.Print(num % 2 , " ")
 }
+
 
