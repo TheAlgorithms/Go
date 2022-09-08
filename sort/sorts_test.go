5266a16f89e42d1a82c9b6631166c66c1c47c97b
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-09-08 16:51:18 +0500

    test: sorted slice string

--- sort/sorts_test.go
+++ sort/sorts_test.go
@@ -62,6 +62,60 @@ func testFramework(t *testing.T, sortingFunction func([]int) []int) {
 			name:     "Empty Slice",
 		},
 	}
+	sortTestsString := []struct {
+		input    []string
+		expected []string
+		name     string
+	}{
+		//Sorted slice
+		{
+			input:    []string{"a", "b", "c", "d"},
+			expected: []string{"a", "b", "c", "d"},
+			name:     "Sorted Unsigned",
+		},
+		//Reversed slice
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Unsigned",
+		},
+		//Sorted slice
+		{
+			input:    []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Sorted Signed",
+		},
+		//Reversed slice
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Signed",
+		},
+		//Reversed slice, even length
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Signed #2",
+		},
+		//Random order with repetitions
+		{
+			input:    []int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10},
+			name:     "Random order Signed",
+		},
+		//Single-entry slice
+		{
+			input:    []int{1},
+			expected: []int{1},
+			name:     "Singleton",
+		},
+		// Empty slice
+		{
+			input:    []int{},
+			expected: []int{},
+			name:     "Empty Slice",
+		},
+	}
 	for _, test := range sortTestsInt {
 		t.Run(test.name, func(t *testing.T) {
 			actual := sortingFunction(test.input)
