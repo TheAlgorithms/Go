98d6e5585ed983b27c11ccb51861680a1480e81f
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-09-08 17:54:23 +0500

    test: sorted unsigned float

--- sort/sorts_test.go
+++ sort/sorts_test.go
@@ -74,6 +74,73 @@ func testInt(t *testing.T, sortingFunction func([]int) []int) {
 	}
 }
 
+func testFloats(t *testing.T, sortingFunction func([]float32) []float32) {
+	sortTestsInt := []struct {
+		input    []float32
+		expected []float32
+		name     string
+	}{
+		//Sorted slice
+		{
+			input:    []float32{1.23, 2.14, 2.34, 4.67, 5.12, 6.51, 7.09, 7.15, 8.45, 9.13},
+			expected: []float32{1.23, 2.14, 2.34, 4.67, 5.12, 6.51, 7.09, 7.15, 8.45, 9.13},
+			name:     "Sorted Unsigned float",
+		},
+		//Reversed slice
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Unsigned",
+		},
+		//Sorted slice
+		{
+			input:    []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Sorted Signed",
+		},
+		//Reversed slice
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Signed",
+		},
+		//Reversed slice, even length
+		{
+			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			name:     "Reversed Signed #2",
+		},
+		//Random order with repetitions
+		{
+			input:    []int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
+			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10},
+			name:     "Random order Signed",
+		},
+		//Single-entry slice
+		{
+			input:    []int{1},
+			expected: []int{1},
+			name:     "Singleton",
+		},
+		// Empty slice
+		{
+			input:    []int{},
+			expected: []int{},
+			name:     "Empty Slice",
+		},
+	}
+	for _, test := range sortTestsInt {
+		t.Run(test.name, func(t *testing.T) {
+			actual := sortingFunction(test.input)
+			sorted := reflect.DeepEqual(actual, test.expected)
+			if !sorted {
+				t.Errorf("test %s failed", test.name)
+				t.Errorf("actual %v expected %v", actual, test.expected)
+			}
+		})
+	}
+}
+
 func testStrings(t *testing.T, sortingFunction func([]string) []string) {
 	sortTestsString := []struct {
 		input    []string
