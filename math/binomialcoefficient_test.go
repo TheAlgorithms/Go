5d7393df82eee17e280a5b39f16499ad3c37c882
Akshay Dubey <akshay.dubey055@gmail.com>
2022-09-13 11:54:26 +0530

    test: Add detailed unit tests for binomial coefficient

--- math/binomialcoefficient_test.go
+++ math/binomialcoefficient_test.go
@@ -6,6 +6,8 @@
 package math_test
 
 import (
+	"errors"
+	"strings"
 	"testing"
 
 	"github.com/TheAlgorithms/Go/math"
@@ -25,10 +27,20 @@ func TestC(t *testing.T) {
 		{-1, -1, -1},
 	}
 	for _, test := range tests {
+		var expectedError error = errors.New("arguments must be positive")
 		result, error := math.C(test.n, test.k)
 		t.Log(test.n, " ", result, " ", error)
 		if result != test.expected {
-			t.Errorf("Wrong result! Expected:%v, Returned:%v, Error:%v", test.expected, result, error.Error())
+			t.Errorf("Wrong result! Expected:%v, Returned:%v", test.expected, result)
+		}
+		if result == -1 && error == nil {
+			t.Errorf("Wrong result! Expected:%v, Returned:%v", expectedError, error.Error())
+		}
+		if result != -1 && error != nil {
+			t.Errorf("Wrong result! Expected:%v, Returned:%v", nil, error.Error())
+		}
+		if result == -1 && !strings.EqualFold(expectedError.Error(), error.Error()) {
+			t.Errorf("Wrong result! Expected:%v, Returned:%v", expectedError.Error(), error.Error())
 		}
 	}
 }
