3e5b2254f19168cf5f0c1812e070f1179558a239
Akshay Dubey <akshay.dubey055@gmail.com>
2022-09-14 11:01:34 +0530

    refactor: Add t.Run block

--- math/binomialcoefficient_test.go
+++ math/binomialcoefficient_test.go
@@ -27,21 +27,23 @@ func TestCombinations(t *testing.T) {
 		{-1, -1, -1},
 	}
 	for _, test := range tests {
-		var expectedError error = errors.New("arguments must be positive")
-		result, error := math.Combinations(test.n, test.k)
-		t.Log(test.n, " ", result, " ", error)
-		if result != test.expected {
-			t.Errorf("Wrong result! Expected:%v, Returned:%v", test.expected, result)
-		}
-		if result == -1 && error == nil {
-			t.Errorf("Wrong result! Expected:%v, Returned:%v", expectedError, error.Error())
-		}
-		if result != -1 && error != nil {
-			t.Errorf("Wrong result! Expected:%v, Returned:%v", nil, error.Error())
-		}
-		if result == -1 && !strings.EqualFold(expectedError.Error(), error.Error()) {
-			t.Errorf("Wrong result! Expected:%v, Returned:%v", expectedError.Error(), error.Error())
-		}
+		t.Run("", func(t *testing.T) {
+			var expectedError error = errors.New("arguments must be positive")
+			result, error := math.Combinations(test.n, test.k)
+			t.Log(test.n, " ", result, " ", error)
+			if result != test.expected {
+				t.Errorf("Wrong result! Expected:%v, Returned:%v", test.expected, result)
+			}
+			if result == -1 && error == nil {
+				t.Errorf("Wrong result! Expected:%v, Returned:%v", expectedError, error.Error())
+			}
+			if result != -1 && error != nil {
+				t.Errorf("Wrong result! Expected:%v, Returned:%v", nil, error.Error())
+			}
+			if result == -1 && !strings.EqualFold(expectedError.Error(), error.Error()) {
+				t.Errorf("Wrong result! Expected:%v, Returned:%v", expectedError.Error(), error.Error())
+			}
+		})
 	}
 }
 func BenchmarkCombinations(b *testing.B) {
