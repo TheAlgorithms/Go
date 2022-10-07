d1f46c523b47c7b2d7c095697250bcf69c64d32c
phantomnat <w.nattadej@gmail.com>
2022-10-07 21:36:52 +0700

    refactor: generic heapsort

--- sort/heapsort.go
+++ sort/heapsort.go
@@ -1,134 +1,40 @@
 package sort
 
-type MaxHeap struct {
-	slice    []Comparable
-	heapSize int
-	indices  map[int]int
-}
+import "github.com/TheAlgorithms/Go/constraints"
 
-func buildMaxHeap(slice0 []int) MaxHeap {
-	var slice []Comparable
-	for _, i := range slice0 {
-		slice = append(slice, Int(i))
+func heapify[T constraints.Ordered](slice []T, N, i int) {
+	largest := i
+	l := 2*i + 1
+	r := 2*i + 2
+
+	if l < N && slice[largest] < slice[l] {
+		largest = l
 	}
-	h := MaxHeap{}
-	h.Init(slice)
-	return h
-}
-
-func (h *MaxHeap) Init(slice []Comparable) {
-	if slice == nil {
-		slice = make([]Comparable, 0)
+	if r < N && slice[largest] < slice[r] {
+		largest = r
 	}
 
-	h.slice = slice
-	h.heapSize = len(slice)
-	h.indices = make(map[int]int)
-	h.Heapify()
-}
+	// change root, if needed
+	if largest != i {
+		slice[i], slice[largest] = slice[largest], slice[i]
 
-func (h MaxHeap) Heapify() {
-	for i, v := range h.slice {
-		h.indices[v.Idx()] = i
-	}
-	for i := h.heapSize / 2; i >= 0; i-- {
-		h.heapifyDown(i)
+		// heapify the root
+		heapify(slice, N, largest)
 	}
 }
 
-func (h *MaxHeap) Pop() Comparable {
-	if h.heapSize == 0 {
-		return nil
+func HeapSort[T constraints.Ordered](slice []T) []T {
+	N := len(slice)
+
+	// build a maxheap
+	for i := N/2 - 1; i >= 0; i-- {
+		heapify(slice, N, i)
 	}
 
-	i := h.slice[0]
-	h.heapSize--
-
-	h.slice[0] = h.slice[h.heapSize]
-	h.updateidx(0)
-	h.heapifyDown(0)
-
-	h.slice = h.slice[0:h.heapSize]
-	return i
-}
-
-func (h *MaxHeap) Push(i Comparable) {
-	h.slice = append(h.slice, i)
-	h.updateidx(h.heapSize)
-	h.heapifyUp(h.heapSize)
-	h.heapSize++
-}
-
-func (h MaxHeap) Size() int {
-	return h.heapSize
-}
-
-func (h MaxHeap) Update(i Comparable) {
-	h.slice[h.indices[i.Idx()]] = i
-	h.heapifyUp(h.indices[i.Idx()])
-	h.heapifyDown(h.indices[i.Idx()])
-}
-
-func (h MaxHeap) updateidx(i int) {
-	h.indices[h.slice[i].Idx()] = i
-}
-
-func (h MaxHeap) heapifyUp(i int) {
-	if i == 0 {
-		return
-	}
-	p := i / 2
-
-	if h.slice[i].More(h.slice[p]) {
-		h.slice[i], h.slice[p] = h.slice[p], h.slice[i]
-		h.updateidx(i)
-		h.updateidx(p)
-		h.heapifyUp(p)
-	}
-}
-
-func (h MaxHeap) heapifyDown(i int) {
-	l, r := 2*i+1, 2*i+2
-	max := i
-
-	if l < h.heapSize && h.slice[l].More(h.slice[max]) {
-		max = l
-	}
-	if r < h.heapSize && h.slice[r].More(h.slice[max]) {
-		max = r
-	}
-	if max != i {
-		h.slice[i], h.slice[max] = h.slice[max], h.slice[i]
-		h.updateidx(i)
-		h.updateidx(max)
-		h.heapifyDown(max)
-	}
-}
-
-type Comparable interface {
-	Idx() int
-	More(any) bool
-}
-type Int int
-
-func (a Int) More(b any) bool {
-	return a > b.(Int)
-}
-func (a Int) Idx() int {
-	return int(a)
-}
-
-func HeapSort(slice []int) []int {
-	h := buildMaxHeap(slice)
-	for i := len(h.slice) - 1; i >= 1; i-- {
-		h.slice[0], h.slice[i] = h.slice[i], h.slice[0]
-		h.heapSize--
-		h.heapifyDown(0)
+	for i := N - 1; i > 0; i-- {
+		slice[i], slice[0] = slice[0], slice[i]
+		heapify(slice, i, 0)
 	}
 
-	res := []int{}
-	for _, i := range h.slice {
-		res = append(res, int(i.(Int)))
-	}
-	return res
+	return slice
 }
--- sort/sorts_test.go
+++ sort/sorts_test.go
@@ -1,67 +1,137 @@
 package sort_test
 
 import (
-	"github.com/TheAlgorithms/Go/sort"
 	"reflect"
 	"testing"
+
+	"github.com/TheAlgorithms/Go/constraints"
+	"github.com/TheAlgorithms/Go/sort"
 )
 
+type testData[T constraints.Ordered] struct {
+	input    []T
+	expected []T
+	name     string
+}
+
+var intTestData = []testData[int]{
+	//Sorted slice
+	{
+		input:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		name:     "Sorted Unsigned",
+	},
+	//Reversed slice
+	{
+		input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+		expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		name:     "Reversed Unsigned",
+	},
+	//Sorted slice
+	{
+		input:    []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		name:     "Sorted Signed",
+	},
+	//Reversed slice
+	{
+		input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
+		expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		name:     "Reversed Signed",
+	},
+	//Reversed slice, even length
+	{
+		input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
+		expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		name:     "Reversed Signed #2",
+	},
+	//Random order with repetitions
+	{
+		input:    []int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
+		expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10},
+		name:     "Random order Signed",
+	},
+	//Single-entry slice
+	{
+		input:    []int{1},
+		expected: []int{1},
+		name:     "Singleton",
+	},
+	// Empty slice
+	{
+		input:    []int{},
+		expected: []int{},
+		name:     "Empty Slice",
+	},
+}
+
+var floatTestData = []testData[float32]{
+	//Sorted slice
+	{
+		input:    []float32{1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0},
+		expected: []float32{1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0},
+		name:     "Sorted float32",
+	},
+	//Reversed slice
+	{
+		input:    []float32{2.0, 1.9, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1},
+		expected: []float32{1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0},
+		name:     "Reversed float32",
+	},
+	//Sorted slice
+	{
+		input:    []float32{-2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0},
+		expected: []float32{-2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0},
+		name:     "Sorted float32",
+	},
+	//Reversed slice
+	{
+		input:    []float32{2.0, 1.9, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1, 0, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7, -1.8, -1.9, -2.0},
+		expected: []float32{-2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, 0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0},
+		name:     "Reversed float32",
+	},
+	//Reversed slice, even length
+	{
+		input:    []float32{2.0, 1.9, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7, -1.8, -1.9, -2.0},
+		expected: []float32{-2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0},
+		name:     "Reversed float32 #2",
+	},
+	//Random order with repetitions
+	{
+		input:    []float32{-1.5, 1.7, 1.4, -1.2, 1.6, 1.5, 1.8, 1.3, 1.2, -1.7, -1.1, 0, -1.3, 1.9, -1.6, -1.4, 2, 1.9, 1.1, -1.8, -1.9, -2},
+		expected: []float32{-2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, 0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 1.9, 2},
+		name:     "Random order float32",
+	},
+	//Single-entry slice
+	{
+		input:    []float32{1.0},
+		expected: []float32{1.0},
+		name:     "Singleton",
+	},
+	// Empty slice
+	{
+		input:    []float32{},
+		expected: []float32{},
+		name:     "Empty Slice",
+	},
+}
+
 func testFramework(t *testing.T, sortingFunction func([]int) []int) {
-	sortTests := []struct {
-		input    []int
-		expected []int
-		name     string
-	}{
-		//Sorted slice
-		{
-			input:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			name:     "Sorted Unsigned",
-		},
-		//Reversed slice
-		{
-			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
-			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			name:     "Reversed Unsigned",
-		},
-		//Sorted slice
-		{
-			input:    []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			name:     "Sorted Signed",
-		},
-		//Reversed slice
-		{
-			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
-			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			name:     "Reversed Signed",
-		},
-		//Reversed slice, even length
-		{
-			input:    []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10},
-			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			name:     "Reversed Signed #2",
-		},
-		//Random order with repetitions
-		{
-			input:    []int{-5, 7, 4, -2, 6, 5, 8, 3, 2, -7, -1, 0, -3, 9, -6, -4, 10, 9, 1, -8, -9, -10},
-			expected: []int{-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10},
-			name:     "Random order Signed",
-		},
-		//Single-entry slice
-		{
-			input:    []int{1},
-			expected: []int{1},
-			name:     "Singleton",
-		},
-		// Empty slice
-		{
-			input:    []int{},
-			expected: []int{},
-			name:     "Empty Slice",
-		},
+	for _, test := range intTestData {
+		t.Run(test.name, func(t *testing.T) {
+			actual := sortingFunction(test.input)
+			sorted := reflect.DeepEqual(actual, test.expected)
+			if !sorted {
+				t.Errorf("test %s failed", test.name)
+				t.Errorf("actual %v expected %v", actual, test.expected)
+			}
+		})
 	}
-	for _, test := range sortTests {
+
+}
+
+func genericTestFramework[T constraints.Ordered](t *testing.T, data []testData[T], sortingFunction func([]T) []T) {
+	for _, test := range data {
 		t.Run(test.name, func(t *testing.T) {
 			actual := sortingFunction(test.input)
 			sorted := reflect.DeepEqual(actual, test.expected)
@@ -96,7 +166,8 @@ func TestMergeIter(t *testing.T) {
 }
 
 func TestHeap(t *testing.T) {
-	testFramework(t, sort.HeapSort)
+	genericTestFramework(t, intTestData, sort.HeapSort[int])
+	genericTestFramework(t, floatTestData, sort.HeapSort[float32])
 }
 
 func TestCount(t *testing.T) {
@@ -201,7 +272,7 @@ func BenchmarkMergeIter(b *testing.B) {
 }
 
 func BenchmarkHeap(b *testing.B) {
-	benchmarkFramework(b, sort.HeapSort)
+	benchmarkFramework(b, sort.HeapSort[int])
 }
 
 func BenchmarkCount(b *testing.B) {
