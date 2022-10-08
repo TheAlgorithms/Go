d9dc6d424d95a6a09a0a96b444f3dcb38fb26d90
piotr.idzik <vil02@o2.pl>
2022-10-08 19:02:38 +0200

    refactor: make example data immutable

--- dynamic/rodcutting_test.go
+++ dynamic/rodcutting_test.go
@@ -6,19 +6,23 @@ import (
 	"github.com/TheAlgorithms/Go/dynamic"
 )
 
-var rodCuttingExamples = []struct {
+type RodCuttingExample struct {
 	price    []int
 	length   int
 	expected int
-}{
-	{[]int{0, 1, 5, 8, 9}, 4, 10},
-	{[]int{0, 2, 5, 7, 8, 0}, 5, 12},
-	{[]int{0, 1, 5, 8, 9, 10, 17, 17, 20}, 8, 22},
-	{[]int{0, 3, 5, 8, 9, 10, 17, 17, 20}, 8, 24},
+}
+
+func getRodCuttingExamples() []RodCuttingExample {
+	return []RodCuttingExample{
+		{[]int{0, 1, 5, 8, 9}, 4, 10},
+		{[]int{0, 2, 5, 7, 8, 0}, 5, 12},
+		{[]int{0, 1, 5, 8, 9, 10, 17, 17, 20}, 8, 22},
+		{[]int{0, 3, 5, 8, 9, 10, 17, 17, 20}, 8, 24},
+	}
 }
 
 func cutRobSolTestFunc(t *testing.T, cutRodSolFunc func([]int, int) int) {
-	for _, tc := range rodCuttingExamples {
+	for _, tc := range getRodCuttingExamples() {
 		actual := cutRodSolFunc(tc.price, tc.length)
 		if actual != tc.expected {
 			t.Errorf("for the price: %d and length %d expected %d, but got: %d", tc.price, tc.length, tc.expected, actual)
