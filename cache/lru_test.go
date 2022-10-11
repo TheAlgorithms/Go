34e7ab92b47d01c33e9349d90eb948e1dc58db26
Chanwit Piromplad <kingkong2103@gmail.com>
2022-10-11 16:31:18 +0700

    Apply suggestions from code review
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>

--- cache/lru_test.go
+++ cache/lru_test.go
@@ -15,29 +15,29 @@ func TestLRU(t *testing.T) {
 		got := cache.Get(key)
 
 		if got != value {
-			t.Errorf("Put: %v, Got: %v", value, got)
+			t.Errorf("expected: %v, got: %v", value, got)
 		}
 	})
 
-	t.Run("Test 2: Get data without cache should got nil", func(t *testing.T) {
+	t.Run("Test 2: Get data not stored in cache should return nil", func(t *testing.T) {
 		got := cache.Get("2")
 		if got != nil {
-			t.Errorf("Got: %v", got)
+			t.Errorf("expected: nil, got: %v", got)
 		}
 	})
 
-	t.Run("Test 3: Put data over capacity and Get should got nil", func(t *testing.T) {
+	t.Run("Test 3: Put data over capacity and Get should return nil", func(t *testing.T) {
 		cache.Put("2", 2)
 		cache.Put("3", 3)
 		cache.Put("4", 4)
 
 		got := cache.Get("1")
 		if got != nil {
-			t.Errorf("Got: %v", got)
+			t.Errorf("expected: nil, got: %v", got)
 		}
 	})
 
-	t.Run("test 4: Put key over capacity but key already existing", func(t *testing.T) {
+	t.Run("test 4: Put key over capacity but recent key exists", func(t *testing.T) {
 		cache.Put("4", 4)
 		cache.Put("3", 3)
 		cache.Put("2", 2)
@@ -45,14 +45,14 @@ func TestLRU(t *testing.T) {
 
 		got := cache.Get("4")
 		if got != nil {
-			t.Errorf("Got: %v", got)
+			t.Errorf("expected: nil, got: %v", got)
 		}
 
 		expected := 3
 		got = cache.Get("3")
 
 		if got != expected {
-			t.Errorf("Expected: %v, Got: %v", expected, got)
+			t.Errorf("expected: %v, got: %v", expected, got)
 		}
 
 	})
