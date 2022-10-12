44b39e09ddf43a23fde630679c9bad20948b422b
Vũ Hải Lâm <lamvh2812@gmail.com>
2022-10-12 11:51:00 +0700

    feat: Use generic for pigeonholesort and max min Int  (#565)

--- math/min/min.go
+++ math/min/min.go
@@ -1,7 +1,9 @@
 package min
 
+import "github.com/TheAlgorithms/Go/constraints"
+
 // Int is a function which returns the minimum of all the integers provided as arguments.
-func Int(values ...int) int {
+func Int[T constraints.Integer](values ...T) T {
 	min := values[0]
 	for _, value := range values {
 		if value < min {
