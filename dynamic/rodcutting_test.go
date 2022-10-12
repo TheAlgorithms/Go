bcd0db23bbdd9f32ad5d0b60e816bf39217d1cc8
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-10-12 13:17:13 +0200

    tests: fix the log message

--- dynamic/rodcutting_test.go
+++ dynamic/rodcutting_test.go
@@ -25,9 +25,7 @@ func cutRodSolTestFunc(t *testing.T, cutRodSolFunc func([]int, int) int) {
 	for _, tc := range getRodCuttingTestCases() {
 		actual := cutRodSolFunc(tc.price, tc.length)
 		if actual != tc.expected {
-			t.Errorf(
-				"expected: %d,  got: %d",
-				tc.price, tc.length, tc.expected, actual)
+			t.Errorf("expected: %d, got: %d", tc.expected, actual)
 		}
 	}
 }
