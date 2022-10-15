03c8ce84d6b45153b4709997c4c1e7d54d5792c4
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-10-15 21:50:08 +0200

    tests: Modular Inverse (#570)
    * test: add missing test cases
    
    * fix: handle the case b == 0
    
    * style: fix typos
    
    * style: remove duplicated code
    
    * style: rename b to m
    
    * style: remove misleading comment

--- math/modular/inverse.go
+++ math/modular/inverse.go
@@ -13,15 +13,14 @@ import (
 	"github.com/TheAlgorithms/Go/math/gcd"
 )
 
-// ErrorIntOverflow For asserting that the values do not overflow in Int64
 var ErrorInverse = errors.New("no Modular Inverse exists")
 
 // Inverse Modular function
-func Inverse(a, b int64) (int64, error) {
-	gcd, x, _ := gcd.Extended(a, b)
-	if gcd != 1 {
+func Inverse(a, m int64) (int64, error) {
+	gcd, x, _ := gcd.Extended(a, m)
+	if gcd != 1 || m == 0 {
 		return 0, ErrorInverse
 	}
 
-	return ((b + (x % b)) % b), nil // this is necessary because of Go's use of architecture specific instruction for the % operator.
+	return ((m + (x % m)) % m), nil // this is necessary because of Go's use of architecture specific instruction for the % operator.
 }
