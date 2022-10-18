cff6ea5fbe11e55666fb99dfa1b18b368c825264
Chetan Patil <chetan.patil2@ingka.ikea.com>
2022-10-18 22:21:40 +0200

    fix: review comments incorporated

--- math/geometry/distancebetweenpoints_test.go
+++ math/geometry/distancebetweenpoints_test.go
@@ -1,46 +1,58 @@
 package geometry
 
-import "testing"
+import (
+	"testing"
+)
 
 type args struct {
-	x1 float64
-	y1 float64
-	x2 float64
-	y2 float64
+	p1 CartesianPoint
+	p2 CartesianPoint
 }
 
 func TestFindDistanceBetweenTwoPoints(t *testing.T) {
 	tests := []struct {
-		name string
-		args args
-		want float64
+		name    string
+		args    args
+		want    float64
+		wantErr bool
 	}{
 		{
 			"(0,0) and (2,-2)",
 			args{
-				0, 0, 2, -2,
+				CartesianPoint{0, 0},
+				CartesianPoint{2, -2},
 			},
 			2.8284271247461903,
+			false,
 		},
 		{
 			"(-20,23) and (-15,68)",
 			args{
-				-20, 23, -15, 68,
+				CartesianPoint{-20, 23},
+				CartesianPoint{-15, 68},
 			},
 			45.27692569068709,
+			false,
 		},
 		{
 			"(2,2) and (14,11)",
 			args{
-				2, 2, 14, 11,
+				CartesianPoint{2, 2},
+				CartesianPoint{14, 11},
 			},
 			15,
+			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := FindDistanceBetweenTwoPoints(tt.args.x1, tt.args.y1, tt.args.x2, tt.args.y2); got != tt.want {
-				t.Errorf("FindDistanceBetweenTwoPoints() = %v, want %v", got, tt.want)
+			got, err := FindDistanceBetweenTwoPoints(tt.args.p1, tt.args.p2)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EuclideanDistance() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("EuclideanDistance() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -48,37 +60,44 @@ func TestFindDistanceBetweenTwoPoints(t *testing.T) {
 
 func BenchmarkFindDistanceBetweenTwoPoints(b *testing.B) {
 	benchmarks := []struct {
-		name     string
-		args     args
-		expected float64
+		name    string
+		args    args
+		want    float64
+		wantErr bool
 	}{
 		{
 			"(0,0) and (2,-2)",
 			args{
-				0, 0, 2, -2,
+				CartesianPoint{0, 0},
+				CartesianPoint{2, -2},
 			},
 			2.8284271247461903,
+			false,
 		},
 		{
 			"(-20,23) and (-15,68)",
 			args{
-				-20, 23, -15, 68,
+				CartesianPoint{-20, 23},
+				CartesianPoint{-15, 68},
 			},
 			45.27692569068709,
+			false,
 		},
 		{
 			"(2,2) and (14,11)",
 			args{
-				2, 2, 14, 11,
+				CartesianPoint{2, 2},
+				CartesianPoint{14, 11},
 			},
 			15,
+			false,
 		},
 	}
 
 	for _, bm := range benchmarks {
 		b.Run(bm.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				FindDistanceBetweenTwoPoints(bm.args.x1, bm.args.y1, bm.args.x2, bm.args.x2)
+				FindDistanceBetweenTwoPoints(bm.args.p1, bm.args.p2)
 			}
 		})
 	}
