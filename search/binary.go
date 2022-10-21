8ffdb6dd4d21b9776d421e53f3acbe6c043abf36
POOJA BHOSKAR <72018117+bhoskarpooja@users.noreply.github.com>
2022-10-21 15:40:11 +0530

    Update binary.go

--- search/binary.go
+++ search/binary.go
@@ -1,82 +1,30 @@
-package search
+package main
+import "fmt"
 
-// Binary search for target within a sorted array by repeatedly dividing the array in half and comparing the midpoint with the target.
-// This function uses recursive call to itself.
-// If a target is found, the index of the target is returned. Else the function return -1 and ErrNotFound.
-func Binary(array []int, target int, lowIndex int, highIndex int) (int, error) {
-	if highIndex < lowIndex || len(array) == 0 {
-		return -1, ErrNotFound
-	}
-	mid := int(lowIndex + (highIndex-lowIndex)/2)
-	if array[mid] > target {
-		return Binary(array, target, lowIndex, mid-1)
-	} else if array[mid] < target {
-		return Binary(array, target, mid+1, highIndex)
-	} else {
-		return mid, nil
-	}
-}
+func binarySearch(needle int, haystack []int) bool {
 
-// BinaryIterative search for target within a sorted array by repeatedly dividing the array in half and comparing the midpoint with the target.
-// Unlike Binary, this function uses iterative method and not recursive.
-// If a target is found, the index of the target is returned. Else the function return -1 and ErrNotFound.
-func BinaryIterative(array []int, target int) (int, error) {
-	startIndex := 0
-	endIndex := len(array) - 1
-	var mid int
-	for startIndex <= endIndex {
-		mid = int(startIndex + (endIndex-startIndex)/2)
-		if array[mid] > target {
-			endIndex = mid - 1
-		} else if array[mid] < target {
-			startIndex = mid + 1
-		} else {
-			return mid, nil
-		}
-	}
-	return -1, ErrNotFound
-}
+	low := 0
+	high := len(haystack) - 1
 
-// Returns index to the first element in the range [0, len(array)-1] that is not less than (i.e. greater or equal to) target.
-// return -1 and ErrNotFound if no such element is found.
-func LowerBound(array []int, target int) (int, error) {
-	startIndex := 0
-	endIndex := len(array) - 1
-	var mid int
-	for startIndex <= endIndex {
-		mid = int(startIndex + (endIndex-startIndex)/2)
-		if array[mid] < target {
-			startIndex = mid + 1
-		} else {
-			endIndex = mid - 1
+	for low <= high{
+		median := (low + high) / 2
+
+		if haystack[median] < needle {
+			low = median + 1
+		}else{
+			high = median - 1
 		}
 	}
 
-	//when target greater than every element in array, startIndex will out of bounds
-	if startIndex >= len(array) {
-		return -1, ErrNotFound
+	if low == len(haystack) || haystack[low] != needle {
+		return false
 	}
-	return startIndex, nil
+
+	return true
 }
 
-// Returns index to the first element in the range [lowIndex, len(array)-1] that is greater than target.
-// return -1 and ErrNotFound if no such element is found.
-func UpperBound(array []int, target int) (int, error) {
-	startIndex := 0
-	endIndex := len(array) - 1
-	var mid int
-	for startIndex <= endIndex {
-		mid = int(startIndex + (endIndex-startIndex)/2)
-		if array[mid] > target {
-			endIndex = mid - 1
-		} else {
-			startIndex = mid + 1
-		}
-	}
 
-	//when target greater or equal than every element in array, startIndex will out of bounds
-	if startIndex >= len(array) {
-		return -1, ErrNotFound
-	}
-	return startIndex, nil
+func main(){
+	items := []int{1,2, 9, 20, 31, 45, 63, 70, 100}
+	fmt.Println(binarySearch(63, items))
 }
