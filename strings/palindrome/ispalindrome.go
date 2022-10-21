ccf2b2f50d64677cd2772cd1954e711e01f9c305
POOJA BHOSKAR <72018117+bhoskarpooja@users.noreply.github.com>
2022-10-21 15:44:46 +0530

    Update ispalindrome.go

--- strings/palindrome/ispalindrome.go
+++ strings/palindrome/ispalindrome.go
@@ -1,55 +1,22 @@
-// ispalindrome.go
-// description: Checks if a given string is palindrome or not
-// details:
-// Palindromes are expressions that read the same way forwards and backwards.
-// They can be words/phrases (like "racecar" and "Do geese see God?"), or even
-// numbers (like "02/02/2020"). Usually punctuation signs, capitalization
-// and spaces are ignored. A regular expression was used to achieve that.
-// See more information on: https://en.wikipedia.org/wiki/Palindrome
-// author(s) [Fernanda Kawasaki](https://github.com/fernandakawasaki)
-// see ispalindrome_test.go
+package main
 
-package palindrome
+import "fmt"
 
-import (
-	"regexp"
-	"strings"
-)
+func main() {
 
-func cleanString(text string) string {
-	clean_text := strings.ToLower(text)
-	clean_text = strings.Join(strings.Fields(clean_text), "") // Remove spaces
-	regex, _ := regexp.Compile(`[^\p{L}\p{N} ]+`)             // Regular expression for alphanumeric only characters
-	return regex.ReplaceAllString(clean_text, "")
-}
+	var originalNumber int = 12321
+        var reverseNumber int = 0
+	var temp = originalNumber
 
-func IsPalindrome(text string) bool {
-	clean_text := cleanString(text)
-	var i, j int
-	rune := []rune(clean_text)
-	for i = 0; i < len(rune)/2; i++ {
-		j = len(rune) - 1 - i
-		if string(rune[i]) != string(rune[j]) {
-			return false
-		}
+	for temp > 0 {
+		var remainder = temp % 10
+		reverseNumber = (reverseNumber * 10) + remainder
+		temp = temp / 10
 	}
-	return true
-}
 
-func IsPalindromeRecursive(text string) bool {
-	clean_text := cleanString(text)
-	runes := []rune(clean_text)
-	return isPalindromeRecursiveHelper(runes, 0, int64(len(runes)))
-}
-
-func isPalindromeRecursiveHelper(runes []rune, start int64, end int64) bool {
-	if start >= end {
-		return true
+	if originalNumber == reverseNumber {
+		fmt.Println("The given number is Palindrome");
+	} else {
+		fmt.Println("The given number is NOT a Palindrome");
 	}
-	if runes[start] != runes[end-1] {
-		return false
-	}
-	start = start + 1
-	end = end - 1
-	return isPalindromeRecursiveHelper(runes, start, end)
 }
