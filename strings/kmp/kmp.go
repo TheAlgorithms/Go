eaec0867e0bc0f9a5de4fde622f131a435e873a4
uh-zz <uhzz.contact@gmail.com>
2022-10-22 20:18:03 +0900

    add: kmp algorithm

--- strings/kmp/kmp.go
+++ strings/kmp/kmp.go
@@ -1,64 +1,49 @@
 package kmp
 
-import (
-	"fmt"
-)
-
-const notFoundPosition int = -1
-
-type Result struct {
-	resultPosition     int
-	numberOfComparison int
-}
-
 // Kmp Function kmp performing the Knuth-Morris-Pratt algorithm.
-// Prints whether the word/pattern was found and on what position in the text or not.
-// m - current match in text, i - current character in w, c - amount of comparisons.
-func Kmp(text string, word string) Result {
-	m, i, c := 0, 0, 0
-	t := kmpTable(word)
-	for m+i < len(text) {
-		fmt.Printf("\n   comparing characters %c %c at positions %d %d", text[m+i], word[i], m+i, i)
-		c++
-		if word[i] == text[m+i] {
-			fmt.Printf(" - match")
-			if i == len(word)-1 {
-				return Result{
-					m, c,
-				}
+func Kmp(word, text string, patternTable []int) []int {
+	if len(word) > len(text) {
+		return nil
+	}
+
+	var (
+		i, j    int
+		matches []int
+	)
+	for i+j < len(text) {
+
+		if word[j] == text[i+j] {
+			j++
+			if j == len(word) {
+				matches = append(matches, i)
+
+				i = i + j
+				j = 0
 			}
-			i++
 		} else {
-			m = m + i - t[i]
-			if t[i] > -1 {
-				i = t[i]
+			i = i + j - patternTable[j]
+			if patternTable[j] > -1 {
+				j = patternTable[j]
 			} else {
-				i = 0
+				j = 0
 			}
 		}
 	}
-	return Result{notFoundPosition,
-		c,
-	}
+	return matches
 }
 
-// Table building algorithm.
-// Takes word to be analyzed and table to be filled.
-func kmpTable(word string) (t []int) {
-	t = make([]int, len(word))
-	pos, cnd := 2, 0
-	t[0], t[1] = -1, 0
-	for pos < len(word) {
-		if word[pos-1] == word[cnd] {
-			cnd++
-			t[pos] = cnd
-			pos++
-		} else if cnd > 0 {
-			cnd = t[cnd]
-		} else {
-			t[pos] = 0
-			pos++
+// Table building for kmp algorithm.
+func Table(w string) []int {
+	var (
+		t []int = []int{-1}
+		k int
+	)
+	for j := 1; j < len(w); j++ {
+		k = j - 1
+		for w[0:k] != w[j-k:j] && k > 0 {
+			k--
 		}
+		t = append(t, k)
 	}
 	return t
 }
