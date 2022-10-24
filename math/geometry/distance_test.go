afe4a2f15f40cbb1fcb02da9850b097e1b2c6a9b
Chetan Patil <chetan.patil2@ingka.ikea.com>
2022-10-24 22:38:22 +0200

    test: added test for error, updated benchmark test

--- math/geometry/distance_test.go
+++ math/geometry/distance_test.go
@@ -43,6 +43,15 @@ func TestFindDistanceBetweenTwoPoints(t *testing.T) {
 			15,
 			false,
 		},
+		{
+			"Return error for mismatched dimensions(2,2) and ()",
+			args{
+				EuclideanPoint{2, 2},
+				EuclideanPoint{},
+			},
+			-1,
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -59,46 +68,10 @@ func TestFindDistanceBetweenTwoPoints(t *testing.T) {
 }
 
 func BenchmarkFindDistanceBetweenTwoPoints(b *testing.B) {
-	benchmarks := []struct {
-		name    string
-		args    args
-		want    float64
-		wantErr bool
-	}{
-		{
-			"(0,0) and (2,-2)",
-			args{
-				EuclideanPoint{0, 0},
-				EuclideanPoint{2, -2},
-			},
-			2.8284271247461903,
-			false,
-		},
-		{
-			"(-20,23) and (-15,68)",
-			args{
-				EuclideanPoint{-20, 23},
-				EuclideanPoint{-15, 68},
-			},
-			45.27692569068709,
-			false,
-		},
-		{
-			"(2,2) and (14,11)",
-			args{
-				EuclideanPoint{2, 2},
-				EuclideanPoint{14, 11},
-			},
-			15,
-			false,
-		},
-	}
+	p1 := EuclideanPoint{0, 0}
+	p2 := EuclideanPoint{2, -2}
 
-	for _, bm := range benchmarks {
-		b.Run(bm.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				_, _ = EuclideanDistance(bm.args.p1, bm.args.p2)
-			}
-		})
+	for i := 0; i < b.N; i++ {
+		_, _ = EuclideanDistance(p1, p2)
 	}
 }
