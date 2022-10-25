8290b339f1e149d01711445e2a7f3df083eb89df
Akshay Dubey <akshay.dubey055@gmail.com>
2022-10-25 19:45:42 +0530

    feat: Add Aliquot Sum implementation

---
+++ math/aliquotsum.go
@@ -0,0 +1,29 @@
+// aliquotsum.go
+// description: Returns s(n)
+// details:
+// the aliquot sum s(n) of a positive integer n
+// is the sum of all proper divisors of n,
+// that is, all divisors of n other than n itself
+// wikipedia: https://en.wikipedia.org/wiki/Aliquot_sum
+// author: Akshay Dubey (https://github.com/itsAkshayDubey)
+// see aliquotsum_test.go
+
+package math
+
+// This function returns s(n) for given number
+func AliquotSum(n int) (int, error) {
+	switch {
+	case n < 0:
+		return 0, ErrPosArgsOnly
+	case n == 0:
+		return 0, ErrNonZeroArgsOnly
+	default:
+		var sum int
+		for i := 1; i < n-1; i++ {
+			if n%i == 0 {
+				sum += i
+			}
+		}
+		return sum, nil
+	}
+}
