0d01b1d6d6253735a4be17a9a442615e26d828dc
Focusucof <imthefirstfocus@gmail.com>
2022-10-26 19:08:33 -0400

    fix: added input validation for encrypt function

--- cipher/a1z26/a1z26.go
+++ cipher/a1z26/a1z26.go
@@ -14,9 +14,15 @@ package a1z26
 import (
 	"strconv"
 	"strings"
+	"regexp"
 )
 
 var encryptionMap = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+// function to check if input is valid
+func isValidInput(input string) bool {
+	return regexp.MustCompile(`^[a-z A-Z]+$`).MatchString(input) // check if input contains only valid letters
+}
 
 // helper function to find the index of a given value in an array
 func indexOfLetter(slice []string, item string) string {
@@ -31,19 +37,23 @@ func indexOfLetter(slice []string, item string) string {
 func Encrypt(input string) string {
 	var result string
 
-	input = strings.ToLower(input)
-	for _, char := range input {
-		if string(char) != " " {
-			result += indexOfLetter(encryptionMap, string(char)) // replace letter with the corresponding number
-			result += "-"                                        // separate letters in the same word by dashes
-		} else {
-			result = result[:len(result)-1] // remove remove dash and replace with a space
-			result += " "
-		}
+	if(isValidInput(input)) {
+		input = strings.ToLower(input)
+		for _, char := range input {
+			if string(char) != " " {
+				result += indexOfLetter(encryptionMap, string(char)) // replace letter with the corresponding number
+				result += "-"                                        // separate letters in the same word by dashes
+			} else {
+				result = result[:len(result)-1] // remove remove dash and replace with a space
+				result += " "
+			}
 
+		}
+		result = result[:len(result)-1] // remove leading dash at end of string
+		return result
+	} else {
+		return "Invalid input"
 	}
-	result = result[:len(result)-1] // remove leading dash at end of string
-	return result
 }
 
 func Decrypt(input string) string {
