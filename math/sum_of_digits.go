4f514638ddde6cafa8469b5f84a90e3019160080
kalyan-arepalle <kalyan.arepalle@razorpay.com>
2022-10-29 18:26:21 +0530

    limit fix

--- math/sum_of_digits.go
+++ math/sum_of_digits.go
@@ -3,7 +3,9 @@ package math
 // SumOfDigits returns sum of digits of the integer
 func SumOfDigits(num int64) (sum int64) {
 	// If num is negative then negative sign is neglected.
-	if num < 0 {
+	// -9223372036854775808 is the min value of int64, but if multiplied by -1 we get
+	// 9223372036854775808 which is > 9223372036854775807, the max value of int64, causing overflow
+	if num < 0 && num > -9223372036854775808 {
 		num = -1 * num
 	}
 
