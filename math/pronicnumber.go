fb715ca3b991f02fc7276998b1cf0d7c16857206
Akshay Dubey <akshay.dubey055@gmail.com>
2022-10-31 11:58:34 +0530

    docs: Documentation fixes

--- math/pronicnumber.go
+++ math/pronicnumber.go
@@ -1,5 +1,6 @@
 // pronicnumber.go
-// description: Returns true if the number is pronic, false otherwise
+// description: Returns true if the number is pronic and false otherwise
+// pronic: Consisting of a base or root or first power plus a higher power.
 // details: A number n is a pronic number if
 // it is equal to product of two consecutive numbers m and m+1.
 // wikipedia: https://en.wikipedia.org/wiki/Pronic_number
@@ -10,7 +11,7 @@ package math
 
 import stdMath "math"
 
-// This function returns true if argument passed to the function is pronic, false otherwise.
+// PronicNumber returns true if argument passed to the function is pronic and false otherwise.
 func PronicNumber(n int) bool {
 	if n < 0 || n%2 == 1 {
 		return false
