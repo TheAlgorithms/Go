7eb041010aed929cbe9140d018e99b2baf5d040b
Akshay Dubey <38462415+itsAkshayDubey@users.noreply.github.com>
2022-10-31 22:18:11 +0530

    algorithm: Aliquot Sum (#594)
    * feat: Add Aliquot Sum implementation
    
    * test: Add test for Aliquot Sum algorithm
    
    * fix: Add separate case for n=1
    
    * refactor: Refactor condition in for loop
    
    Co-authored-by: Rak Laptudirm <rak@laptudirm.com>

---
+++ math/aliquot_test.go
@@ -0,0 +1,39 @@
+// aliquotsum_test.go
+// description: Returns s(n)
+// author: Akshay Dubey (https://github.com/itsAkshayDubey)
+// see aliquotsum.go
+
+package math_test
+
+import (
+	"testing"
+
+	"github.com/TheAlgorithms/Go/math"
+)
+
+func TestAliquotSum(t *testing.T) {
+	var tests = []struct {
+		name          string
+		n             int
+		expectedValue int
+		expectedError error
+	}{
+		{"n = 10", 10, 8, nil},
+		{"n = 11", 11, 1, nil},
+		{"n = -1", -1, 0, math.ErrPosArgsOnly},
+		{"n = 0", 0, 0, math.ErrNonZeroArgsOnly},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := math.AliquotSum(test.n)
+			if result != test.expectedValue || test.expectedError != err {
+				t.Errorf("expected error: %s, got: %s; expected value: %v, got: %v", test.expectedError, err, test.expectedValue, result)
+			}
+		})
+	}
+}
+func BenchmarkAliquotSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = math.AliquotSum(65536)
+	}
+}
