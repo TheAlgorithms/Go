4ff06a8113421b53424bf54a278640ef45fbf710
Akshay Dubey <akshay.dubey055@gmail.com>
2022-11-09 11:28:38 +0530

    docs: Fix details block with proper definition

--- math/pronicnumber.go
+++ math/pronicnumber.go
@@ -1,8 +1,8 @@
 // pronicnumber.go
 // description: Returns true if the number is pronic and false otherwise
-// pronic: Consisting of a base or root or first power plus a higher power.
-// details: A number n is a pronic number if
-// it is equal to product of two consecutive numbers m and m+1.
+// details:
+//  Pronic number: For any integer n, if there exists integer m
+//  such that n = m * (m + 1) then n is called a pronic number.
 // wikipedia: https://en.wikipedia.org/wiki/Pronic_number
 // author: Akshay Dubey (https://github.com/itsAkshayDubey)
 // see pronicnumber_test.go
