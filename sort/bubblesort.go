b069ada23479dfcef81a3deb68e5bb788b99c69a
Joel Sousa <joelfps@gmail.com>
2022-11-24 02:28:21 +0000

    Improve Bubblesort comparions
    During the Bubble sort for N elements, we make
    (N - 1) + (N - 2) + (N - 3) … + 1 comparisons.
    
    *Eg for 5 element array:*
    We should have
    4 + 3 + 2 + 1 = 10 comparisons.
    4 + 3+ 2 + 1 = 10 swaps in the worst cenario.
    
    But with the current implementation we have 
    4+4+4+4 = 16 comparisons if I'm not mistaken.

--- sort/bubblesort.go
+++ sort/bubblesort.go
@@ -8,9 +8,10 @@ import "github.com/TheAlgorithms/Go/constraints"
 // Bubble is a simple generic definition of Bubble sort algorithm.
 func Bubble[T constraints.Ordered](arr []T) []T {
 	swapped := true
+	sortedIdx := 1
 	for swapped {
 		swapped = false
-		for i := 0; i < len(arr)-1; i++ {
+		for i := 0; i < len(arr)-sortedIdx; i++ {
 			if arr[i+1] < arr[i] {
 				arr[i+1], arr[i] = arr[i], arr[i+1]
 				swapped = true
