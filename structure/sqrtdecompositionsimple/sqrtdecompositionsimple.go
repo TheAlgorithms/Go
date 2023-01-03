93bc38df7215a73d74991ac80bc7af3b3a50a652
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-01-03 20:31:21 +0100

    fix style

--- structure/sqrtdecompositionsimple/sqrtdecompositionsimple.go
+++ structure/sqrtdecompositionsimple/sqrtdecompositionsimple.go
@@ -18,78 +18,78 @@ package sqrtdecompositionsimple
 import "math"
 
 type SqrtDecompositionSimple[E any, Q any] struct {
-        querySingleElement func(element E) Q
-        unionQ             func(q1 Q, q2 Q) Q
-        updateQ            func(oldQ Q, oldE E, newE E) (newQ Q)
+	querySingleElement func(element E) Q
+	unionQ             func(q1 Q, q2 Q) Q
+	updateQ            func(oldQ Q, oldE E, newE E) (newQ Q)
 
-        elements  []E
-        blocks    []Q
-        blockSize uint64
+	elements  []E
+	blocks    []Q
+	blockSize uint64
 }
 
 // Assumption: len(elements) > 0
 func NewSqrtDecompositionSimple[E any, Q any](
-        elements []E,
-        querySingleElement func(element E) Q,
-        unionQ func(q1 Q, q2 Q) Q,
-        updateQ func(oldQ Q, oldE E, newE E) (newQ Q),
+	elements []E,
+	querySingleElement func(element E) Q,
+	unionQ func(q1 Q, q2 Q) Q,
+	updateQ func(oldQ Q, oldE E, newE E) (newQ Q),
 ) *SqrtDecompositionSimple[E, Q] {
-        sqrtDec := &SqrtDecompositionSimple[E, Q]{
-                querySingleElement: querySingleElement,
-                unionQ:             unionQ,
-                updateQ:            updateQ,
-                elements:           elements,
-        }
-        sqrt := math.Sqrt(float64(len(sqrtDec.elements)))
-        blockSize := uint64(sqrt)
-        numBlocks := uint64(math.Ceil(float64(len(elements)) / float64(blockSize)))
-        sqrtDec.blocks = make([]Q, numBlocks)
-        for i := uint64(0); i < uint64(len(elements)); i++ {
-                if i%blockSize == 0 {
-                        sqrtDec.blocks[i/blockSize] = sqrtDec.querySingleElement(elements[i])
-                } else {
-                        sqrtDec.blocks[i/blockSize] = sqrtDec.unionQ(sqrtDec.blocks[i/blockSize], sqrtDec.querySingleElement(elements[i]))
-                }
-        }
-        sqrtDec.blockSize = blockSize
-        return sqrtDec
+	sqrtDec := &SqrtDecompositionSimple[E, Q]{
+		querySingleElement: querySingleElement,
+		unionQ:             unionQ,
+		updateQ:            updateQ,
+		elements:           elements,
+	}
+	sqrt := math.Sqrt(float64(len(sqrtDec.elements)))
+	blockSize := uint64(sqrt)
+	numBlocks := uint64(math.Ceil(float64(len(elements)) / float64(blockSize)))
+	sqrtDec.blocks = make([]Q, numBlocks)
+	for i := uint64(0); i < uint64(len(elements)); i++ {
+		if i%blockSize == 0 {
+			sqrtDec.blocks[i/blockSize] = sqrtDec.querySingleElement(elements[i])
+		} else {
+			sqrtDec.blocks[i/blockSize] = sqrtDec.unionQ(sqrtDec.blocks[i/blockSize], sqrtDec.querySingleElement(elements[i]))
+		}
+	}
+	sqrtDec.blockSize = blockSize
+	return sqrtDec
 }
 
 // Assumption: start < end (non included). Both are valid
 func (s *SqrtDecompositionSimple[E, Q]) Query(start uint64, end uint64) Q {
-        firstIndexNextBlock := ((start / s.blockSize) + 1) * s.blockSize
-        q := s.querySingleElement(s.elements[start])
-        if firstIndexNextBlock > end { // if in same block
-                start++
-                for start < end {
-                        q = s.unionQ(q, s.querySingleElement(s.elements[start]))
-                        start++
-                }
-        } else {
-                // left side
-                start++
-                for start < firstIndexNextBlock {
-                        q = s.unionQ(q, s.querySingleElement(s.elements[start]))
-                        start++
-                }
+	firstIndexNextBlock := ((start / s.blockSize) + 1) * s.blockSize
+	q := s.querySingleElement(s.elements[start])
+	if firstIndexNextBlock > end { // if in same block
+		start++
+		for start < end {
+			q = s.unionQ(q, s.querySingleElement(s.elements[start]))
+			start++
+		}
+	} else {
+		// left side
+		start++
+		for start < firstIndexNextBlock {
+			q = s.unionQ(q, s.querySingleElement(s.elements[start]))
+			start++
+		}
 
-                //middle part
-                endBlock := end / s.blockSize
-                for i := firstIndexNextBlock / s.blockSize; i < endBlock; i++ {
-                        q = s.unionQ(q, s.blocks[i])
-                }
+		//middle part
+		endBlock := end / s.blockSize
+		for i := firstIndexNextBlock / s.blockSize; i < endBlock; i++ {
+			q = s.unionQ(q, s.blocks[i])
+		}
 
-                // right part
-                for i := endBlock * s.blockSize; i < end; i++ {
-                        q = s.unionQ(q, s.querySingleElement(s.elements[i]))
-                }
-        }
-        return q
+		// right part
+		for i := endBlock * s.blockSize; i < end; i++ {
+			q = s.unionQ(q, s.querySingleElement(s.elements[i]))
+		}
+	}
+	return q
 }
 
 // Assumption: index is valid
 func (s *SqrtDecompositionSimple[E, Q]) Update(index uint64, newElement E) {
-        i := index / s.blockSize
-        s.blocks[i] = s.updateQ(s.blocks[i], s.elements[index], newElement)
-        s.elements[index] = newElement
+	i := index / s.blockSize
+	s.blocks[i] = s.updateQ(s.blocks[i], s.elements[index], newElement)
+	s.elements[index] = newElement
 }
