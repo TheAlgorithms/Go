f68cad52e2db078ffc69a8491d54ba1d33a1382e
szabo <aleqvids@gmail.com>
2023-01-31 12:06:22 +0300

    fix: refactor math/binary/sqrt.go

--- math/binary/sqrt.go
+++ math/binary/sqrt.go
@@ -13,12 +13,12 @@ import (
 
 const threeHalves = 1.5
 
-func Sqrt(number float32) float32 {
-	var halfNumber, y float32
-	halfNumber = number * 0.5
-	z := math.Float32bits(number)
+func Sqrt(n float32) float32 {
+	var half, y float32
+	half = n * 0.5
+	z := math.Float32bits(n)
 	z = 0x5f3759df - (z >> 1) // floating point bit level hacking
 	y = math.Float32frombits(z)
-	y = y * (threeHalves - (halfNumber * y * y)) // Newton's approximation
+	y = y * (threeHalves - (half * y * y)) // Newton's approximation
 	return 1 / y
 }
