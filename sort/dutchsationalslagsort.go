ef771452594d13c82c92a1d1dc1665b845e9a14d
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-03-03 18:18:14 +0200

    feat: add dutchNationalFlagSort implementation

---
+++ sort/dutchsationalslagsort.go
@@ -0,0 +1,25 @@
+// Implementation of Dutch National Flag Sort algorithm
+// Reference: https://en.wikipedia.org/wiki/Dutch_national_flag_problem
+
+package sort
+
+import "github.com/TheAlgorithms/Go/constraints"
+
+// DutchNationalFlagSort sorts an array of integers with the Dutch National Flag algorithm
+func DutchNationalFlagSort[T constraints.Signed](arr []T) []T {
+	low, mid, high := 0, 0, len(arr)-1
+	for mid <= high {
+		switch arr[mid] {
+		case 0:
+			arr[low], arr[mid] = arr[mid], arr[low]
+			low++
+			mid++
+		case 1:
+			mid++
+		case 2:
+			arr[mid], arr[high] = arr[high], arr[mid]
+			high--
+		}
+	}
+	return arr
+}
