e364b8b0afe5cdf1c2ba57376fb09f5bf98ce233
Aditya Pal <ap37@iitbbs.ac.in>
2023-04-28 17:11:39 +0530

    feat: address code review comments

--- sort/cyclesort.go
+++ sort/cyclesort.go
@@ -10,12 +10,11 @@ import (
 func Cycle[T constraints.Number](arr []T) []T {
 
 	counter, cycle, len := 0, 0, len(arr)
-
 	// Early return if the array too small
 	if len <= 1 {
 		return arr
 	}
-
+	
 	for cycle = 0; cycle < len-1; cycle++ {
 		elem := arr[cycle]
 		// Find total smaller elements to right
@@ -25,37 +24,30 @@ func Cycle[T constraints.Number](arr []T) []T {
 				pos++
 			}
 		}
-
 		// In case this element is already in correct position, let's skip processing
 		if pos == cycle {
 			continue
 		}
-
 		// In case we have same elements, we want to skip to the end of that list as well, ignoring order
 		// This makes the algorithm unstable for composite elements
 		for elem == arr[pos] {
 			pos++
 		}
-
 		// Now let us put the item to it's right position
 		arr[pos], elem = elem, arr[pos]
 
 		//We need to rotate the array till we have reached the start of the cycle again
 		for pos != cycle {
 			pos = cycle
-
 			// Find smaller elements to right again
 			for counter = cycle + 1; counter < len; counter++ {
 				if arr[counter] < elem {
 					pos++
 				}
 			}
-
-			// Ignore all duplicate elements, if any still appear
 			for elem == arr[pos] {
 				pos++
 			}
-
 			//We can do this unconditionally, but the check helps prevent redundant writes to the array
 			if elem != arr[pos] {
 				arr[pos], elem = elem, arr[pos]
