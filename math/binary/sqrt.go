f7ca03bce44b23802e8ca5d7edfa3479ea491a5c
yan-aint-nickname <50468068+yan-aint-nickname@users.noreply.github.com>
2023-09-01 20:14:21 +0300

    Add fast inverse square root algorithm (#647)
    * Add fast inverse square root algorithm
    
    * PR improvements see comments in https://github.com/TheAlgorithms/Go/pull/647
    
    * Update math/binary/fast_inverse_sqrt.go file docstring
    
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>
    
    * Update math/binary/fast_inverse_sqrt.go function documentation
    
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>
    
    * Fix function documentation grammar
    
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Taj <tjgurwara99@users.noreply.github.com>

--- math/binary/sqrt.go
+++ math/binary/sqrt.go
@@ -7,18 +7,4 @@
 
 package binary
 
-import (
-	"math"
-)
-
-const threeHalves = 1.5
-
-func Sqrt(n float32) float32 {
-	var half, y float32
-	half = n * 0.5
-	z := math.Float32bits(n)
-	z = 0x5f3759df - (z >> 1) // floating point bit level hacking
-	y = math.Float32frombits(z)
-	y = y * (threeHalves - (half * y * y)) // Newton's approximation
-	return 1 / y
-}
+func Sqrt(n float32) float32 { return 1 / FastInverseSqrt(n) }
