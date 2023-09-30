278e009eca7e07a108c92ac0570b8291dcfc09a2
Miguel Dalberto <migueldalberto.acc@gmail.com>
2023-09-29 22:30:00 -0300

    change comments

--- sort/cocktailsort.go
+++ sort/cocktailsort.go
@@ -5,31 +5,30 @@ package sort
 
 import "github.com/TheAlgorithms/Go/constraints"
 
+// Cocktail sort is a variation of bubble sort, operating in two directions (beginning to end, end to beginning)
 func Cocktail[T constraints.Integer](arr []T) []T {
-	if len(arr) == 0 { // guard clause for 0 length arrays
+	if len(arr) == 0 {
 		return arr
 	}
 
 	swapped := false
 
 	for ok := true; ok; ok = swapped {
-		for i := 0; i < len(arr)-1; i++ { // loop from array's beginning to end
-			if arr[i] > arr[i+1] { // swap unsorted elements
+		for i := 0; i < len(arr)-1; i++ {
+			if arr[i] > arr[i+1] {
 				arr[i], arr[i+1] = arr[i+1], arr[i]
 				swapped = true
 			}
 		}
 
 		if !swapped {
-			// if swapped == false, first loop didn't find any unsorted elements.
-			// array is sorted
 			break
 		}
 
 		swapped = false
 
-		for i := len(arr) - 1; i > 0; i-- { // loop from array's end to beginning
-			if arr[i] < arr[i-1] { // swap unsorted elements
+		for i := len(arr) - 1; i > 0; i-- {
+			if arr[i] < arr[i-1] {
 				arr[i], arr[i-1] = arr[i-1], arr[i]
 				swapped = true
 			}
