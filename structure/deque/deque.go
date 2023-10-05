743268cb64329d8147d3f9986807e023e16aa5ca
Sayan Bose <sayan.bose.lmg.2000@gmail.com>
2023-10-05 12:11:54 +0530

    Added the necessary changes: 	- Made the member of the DoublyEndedQueue struct private 	- Removed unnecessary indentations from package comment 	- Implemented black box testing 	- Used errors.New() to declare a constant error in case a dequeuing operation is called on an empty deque 	- Exported the methods of DoublyEndedQueue 	- Edited the names of getters to follow go's naming conventions.

--- structure/deque/deque.go
+++ structure/deque/deque.go
@@ -1,91 +1,99 @@
 /*
-	Package deque implements a Double Ended Queue data structure.
+Package deque implements a Double Ended Queue data structure.
 
-	Description:
-				Double Ended Queue is a generalized version of Queue data structure
-				that allows insert and delete at both ends.
+Description:
+	Double Ended Queue is a generalized version of Queue data structure
+	that allows insert and delete at both ends.
 
-	Basic Operations:
-		enQueueFront(): Adds an item at the front of Deque.
-		enQueueRear(): Adds an item at the rear of Deque.
-		deQueueFront(): Deletes an item from front of Deque.
-		deQueueRear(): Deletes an item from rear of Deque.
-		getFront(): Gets the front item from queue.
-		getRear(): Gets the last item from queue.
-		isEmpty(): Checks whether Deque is empty or not.
-		getLength(): Gets the length of Deque.
+Basic Operations:
+	enQueueFront(): Adds an item at the front of Deque.
+	enQueueRear(): Adds an item at the rear of Deque.
+	deQueueFront(): Deletes an item from front of Deque.
+	deQueueRear(): Deletes an item from rear of Deque.
+	getFront(): Gets the front item from queue.
+	getRear(): Gets the last item from queue.
+	isEmpty(): Checks whether Deque is empty or not.
+	getLength(): Gets the length of Deque.
 
-	References:
-		Wikipedia : https://en.wikipedia.org/wiki/Double-ended_queue
-		Github: https://www.geeksforgeeks.org/deque-set-1-introduction-applications/
+References:
+	Wikipedia : https://en.wikipedia.org/wiki/Double-ended_queue
+	Github: https://www.geeksforgeeks.org/deque-set-1-introduction-applications/
 
-	Author: [Sayan](https://github.com/bose-sayan)
+Author: [Sayan](https://github.com/bose-sayan)
 */
 
 package deque
 
-import "fmt"
+import (
+	"errors"
+)
 
-type DoublyEndedQueue struct {
-	Deque []any
+var ErrEmptyDeQueue = errors.New("DoublyEnded queue is empty, so can't perform this operation")
+
+type DoublyEndedQueue[T any] struct {
+	deque []T
 }
 
-func newDeque() *DoublyEndedQueue {
-	return &DoublyEndedQueue{}
+func NewDeque[T any]() *DoublyEndedQueue[T] {
+	return &DoublyEndedQueue[T]{deque: make([]T, 0)}
 }
 
 // enQueueFront adds an item at the front of Deque.
-func (dq *DoublyEndedQueue) enQueueFront(n any) {
-	dq.Deque = append([]any{n}, dq.Deque...)
+func (dq *DoublyEndedQueue[T]) EnQueueFront(item T) {
+	dq.deque = append([]T{item}, dq.deque...)
 }
 
 // enQueueRear adds an item at the rear of Deque.
-func (dq *DoublyEndedQueue) enQueueRear(n any) {
-	dq.Deque = append(dq.Deque, n)
+func (dq *DoublyEndedQueue[T]) EnQueueRear(item T) {
+	dq.deque = append(dq.deque, item)
 }
 
 // deQueueFront deletes an item from front of Deque and returns it.
-func (dq *DoublyEndedQueue) deQueueFront() (any, error) {
-	if len(dq.Deque) == 0 {
-		return nil, fmt.Errorf("DoublyEnded queue is empty so can't dequeue")
+func (dq *DoublyEndedQueue[T]) DeQueueFront() (T, error) {
+	if len(dq.deque) == 0 {
+		var zeroVal T
+		return zeroVal, ErrEmptyDeQueue
 	}
-	data := dq.Deque[0]
-	dq.Deque = dq.Deque[1:]
-	return data, nil
+	frontElement := dq.deque[0]
+	dq.deque = dq.deque[1:]
+	return frontElement, nil
 }
 
 // deQueueRear deletes an item from rear of Deque and returns it.
-func (dq *DoublyEndedQueue) deQueueRear() (any, error) {
-	if len(dq.Deque) == 0 {
-		return nil, fmt.Errorf("DoublyEnded queue is empty so can't dequeue")
+func (dq *DoublyEndedQueue[T]) DeQueueRear() (T, error) {
+	if len(dq.deque) == 0 {
+		var zeroVal T
+		return zeroVal, ErrEmptyDeQueue
 	}
-	data := dq.Deque[len(dq.Deque)-1]
-	dq.Deque = dq.Deque[:len(dq.Deque)-1]
-	return data, nil
+	rearElement := dq.deque[len(dq.deque)-1]
+	dq.deque = dq.deque[:len(dq.deque)-1]
+	return rearElement, nil
 }
 
 // getFront gets the front item from queue.
-func (dq *DoublyEndedQueue) getFront() (any, error) {
-	if (len(dq.Deque)) == 0 {
-		return nil, fmt.Errorf("DoublyEnded queue is empty so can't get front")
+func (dq *DoublyEndedQueue[T]) Front() (T, error) {
+	if (len(dq.deque)) == 0 {
+		var zeroVal T
+		return zeroVal, ErrEmptyDeQueue
 	}
-	return dq.Deque[0], nil
+	return dq.deque[0], nil
 }
 
 // getRear gets the last item from queue.
-func (dq *DoublyEndedQueue) getRear() (any, error) {
-	if (len(dq.Deque)) == 0 {
-		return nil, fmt.Errorf("DoublyEnded queue is empty so can't get rear")
+func (dq *DoublyEndedQueue[T]) Rear() (T, error) {
+	if (len(dq.deque)) == 0 {
+		var zeroVal T
+		return zeroVal, ErrEmptyDeQueue
 	}
-	return dq.Deque[len(dq.Deque)-1], nil
+	return dq.deque[len(dq.deque)-1], nil
 }
 
 // isEmpty checks whether Deque is empty or not.
-func (dq *DoublyEndedQueue) isEmpty() bool {
-	return len(dq.Deque) == 0
+func (dq *DoublyEndedQueue[T]) Empty() bool {
+	return len(dq.deque) == 0
 }
 
 // getLength gets the length of Deque.
-func (dq *DoublyEndedQueue) getLength() int {
-	return len(dq.Deque)
+func (dq *DoublyEndedQueue[T]) Length() int {
+	return len(dq.deque)
 }
--- structure/deque/deque_test.go
+++ structure/deque/deque_test.go
@@ -6,145 +6,444 @@ import (
 	"testing"
 )
 
+// Zero values
+var zeroInt int
+var zeroString string
+
+type QueryStructure[T any] struct {
+	queryType      string
+	parameter      T
+	expectedResult interface{}
+	expectedError  error
+}
+
+type TestCaseData[T any] struct {
+	description string
+	queries     []QueryStructure[T]
+}
+
 func TestDeque(t *testing.T) {
-	d := newDeque()
 
-	// Test empty deque
-	if !d.isEmpty() {
-		t.Errorf("Expected deque to be empty, but it's not")
+	// Test cases with ints as params
+	testCasesInt := []TestCaseData[int]{
+		{
+			description: "Test empty deque",
+			queries: []QueryStructure[int]{
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroInt,
+					expectedResult: true,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Front",
+					parameter:      zeroInt,
+					expectedResult: zeroInt,
+					expectedError:  ErrEmptyDeQueue,
+				},
+				{
+					queryType:      "Rear",
+					parameter:      zeroInt,
+					expectedResult: zeroInt,
+					expectedError:  ErrEmptyDeQueue,
+				},
+				{
+					queryType:      "DeQueueFront",
+					parameter:      zeroInt,
+					expectedResult: zeroInt,
+					expectedError:  ErrEmptyDeQueue,
+				},
+				{
+					queryType:      "DeQueueRear",
+					parameter:      zeroInt,
+					expectedResult: zeroInt,
+					expectedError:  ErrEmptyDeQueue,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroInt,
+					expectedResult: 0,
+					expectedError:  nil,
+				},
+			},
+		},
+		{
+			description: "Test deque with one element",
+			queries: []QueryStructure[int]{
+				{
+					queryType:      "EnQueueFront",
+					parameter:      1,
+					expectedResult: zeroInt,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroInt,
+					expectedResult: false,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Front",
+					parameter:      zeroInt,
+					expectedResult: 1,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Rear",
+					parameter:      zeroInt,
+					expectedResult: 1,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroInt,
+					expectedResult: 1,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "DeQueueFront",
+					parameter:      zeroInt,
+					expectedResult: 1,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroInt,
+					expectedResult: true,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroInt,
+					expectedResult: 0,
+					expectedError:  nil,
+				},
+			},
+		},
+		{
+			description: "Test deque with multiple elements",
+			queries: []QueryStructure[int]{
+				{
+					queryType:      "EnQueueFront",
+					parameter:      1,
+					expectedResult: zeroInt,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "EnQueueFront",
+					parameter:      2,
+					expectedResult: zeroInt,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "EnQueueRear",
+					parameter:      3,
+					expectedResult: zeroInt,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "EnQueueRear",
+					parameter:      4,
+					expectedResult: zeroInt,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroInt,
+					expectedResult: false,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Front",
+					parameter:      zeroInt,
+					expectedResult: 2,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Rear",
+					parameter:      zeroInt,
+					expectedResult: 4,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroInt,
+					expectedResult: 4,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "DeQueueFront",
+					parameter:      zeroInt,
+					expectedResult: 2,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "DeQueueRear",
+					parameter:      zeroInt,
+					expectedResult: 4,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroInt,
+					expectedResult: false,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroInt,
+					expectedResult: 2,
+					expectedError:  nil,
+				},
+			},
+		},
 	}
 
-	// Test enQueue front
-	d.enQueueFront(1)
-	if d.isEmpty() {
-		t.Errorf("Expected deque to not be empty, but it is")
-	}
-	val, err := d.getFront()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 1 {
-		t.Errorf("Expected front value to be 1, but got %v", val)
-	}
-	val, err = d.getRear()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 1 {
-		t.Errorf("Expected rear value to be 1, but got %v", val)
+	// Test cases with strings as params
+	testCasesString := []TestCaseData[string]{
+		{
+			description: "Test one element deque",
+			queries: []QueryStructure[string]{
+				{
+					queryType:      "EnQueueFront",
+					parameter:      "a",
+					expectedResult: zeroString,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroString,
+					expectedResult: false,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Front",
+					parameter:      zeroString,
+					expectedResult: "a",
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Rear",
+					parameter:      zeroString,
+					expectedResult: "a",
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroString,
+					expectedResult: 1,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "DeQueueFront",
+					parameter:      zeroString,
+					expectedResult: "a",
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroString,
+					expectedResult: true,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroString,
+					expectedResult: 0,
+					expectedError:  nil,
+				},
+			},
+		},
+		{
+			description: "Test multiple elements deque",
+			queries: []QueryStructure[string]{
+				{
+					queryType:      "EnQueueFront",
+					parameter:      "a",
+					expectedResult: zeroString,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "EnQueueFront",
+					parameter:      "b",
+					expectedResult: zeroString,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "EnQueueRear",
+					parameter:      "c",
+					expectedResult: zeroString,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "EnQueueRear",
+					parameter:      "d",
+					expectedResult: zeroString,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroString,
+					expectedResult: false,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Front",
+					parameter:      zeroString,
+					expectedResult: "b",
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Rear",
+					parameter:      zeroString,
+					expectedResult: "d",
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroString,
+					expectedResult: 4,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "DeQueueFront",
+					parameter:      zeroString,
+					expectedResult: "b",
+					expectedError:  nil,
+				},
+				{
+					queryType:      "DeQueueRear",
+					parameter:      zeroString,
+					expectedResult: "d",
+					expectedError:  nil,
+				},
+				{
+					queryType:      "IsEmpty",
+					parameter:      zeroString,
+					expectedResult: false,
+					expectedError:  nil,
+				},
+				{
+					queryType:      "Length",
+					parameter:      zeroString,
+					expectedResult: 2,
+					expectedError:  nil,
+				},
+			},
+		},
 	}
 
-	// Test enQueue rear
-	d.enQueueRear(2)
-	if d.isEmpty() {
-		t.Errorf("Expected deque to not be empty, but it is")
-	}
-	val, err = d.getFront()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 1 {
-		t.Errorf("Expected front value to be 1, but got %v", val)
-	}
-	val, err = d.getRear()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 2 {
-		t.Errorf("Expected rear value to be 1, but got %v", val)
-	}
-
-	// Test deQueue front
-	val, err = d.deQueueFront()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 1 {
-		t.Errorf("Expected dequeued front value to be 1, but got %v", val)
-	}
-	if d.isEmpty() {
-		t.Errorf("Expected deque to not be empty, but it is")
-	}
-	val, err = d.deQueueFront()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 2 {
-		t.Errorf("Expected front value to be 2, but got %v", val)
-	}
-
-	// Test deQueue back
-	d.enQueueFront(1)
-	d.enQueueFront(2)
-
-	val, err = d.deQueueRear()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 1 {
-		t.Errorf("Expected dequeued front value to be 1, but got %v", val)
-	}
-	if d.isEmpty() {
-		t.Errorf("Expected deque to not be empty, but it is")
-	}
-	val, err = d.deQueueRear()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 2 {
-		t.Errorf("Expected front value to be 2, but got %v", val)
+	// Run tests with ints
+	for _, testCase := range testCasesInt {
+		t.Run(testCase.description, func(t *testing.T) {
+			dq := NewDeque[int]()
+			for _, query := range testCase.queries {
+				switch query.queryType {
+				case "EnQueueFront":
+					dq.EnQueueFront(query.parameter)
+				case "EnQueueRear":
+					dq.EnQueueRear(query.parameter)
+				case "DeQueueFront":
+					result, err := dq.DeQueueFront()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "DeQueueRear":
+					result, err := dq.DeQueueRear()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "Front":
+					result, err := dq.Front()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "Rear":
+					result, err := dq.Rear()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "IsEmpty":
+					result := dq.Empty()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+				case "Length":
+					result := dq.Length()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v got %v", query.expectedResult, result)
+					}
+				}
+			}
+		})
 	}
 
-	// Test getFront
-	_, err = d.getFront()
-	if err == nil {
-		t.Errorf("Expected empty queue error, but got none")
+	// Run tests with strings
+	for _, testCase := range testCasesString {
+		t.Run(testCase.description, func(t *testing.T) {
+			dq := NewDeque[string]()
+			for _, query := range testCase.queries {
+				switch query.queryType {
+				case "EnQueueFront":
+					dq.EnQueueFront(query.parameter)
+				case "EnQueueRear":
+					dq.EnQueueRear(query.parameter)
+				case "DeQueueFront":
+					result, err := dq.DeQueueFront()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "DeQueueRear":
+					result, err := dq.DeQueueRear()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "Front":
+					result, err := dq.Front()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "Rear":
+					result, err := dq.Rear()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+					if err != query.expectedError {
+						t.Errorf("Expected %v, got %v", query.expectedError, err)
+					}
+				case "IsEmpty":
+					result := dq.Empty()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v, got %v", query.expectedResult, result)
+					}
+				case "Length":
+					result := dq.Length()
+					if result != query.expectedResult {
+						t.Errorf("Expected %v got %v", query.expectedResult, result)
+					}
+				}
+			}
+		})
 	}
-	d.enQueueFront(1)
-	val, err = d.getFront()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 1 {
-		t.Errorf("Expected front value to be 1, but got %v", val)
-	}
-	_, err = d.deQueueFront()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	// Test getRear
-	_, err = d.getRear()
-	if err == nil {
-		t.Errorf("Expected empty queue error, but got none")
-	}
-	d.enQueueRear(1)
-	val, err = d.getRear()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	if val != 1 {
-		t.Errorf("Expected rear value to be 1, but got %v", val)
-	}
-	_, err = d.deQueueRear()
-	if err != nil {
-		t.Errorf("Expected no error, but got %v", err)
-	}
-	// Test empty deque
-	if !d.isEmpty() {
-		t.Errorf("Expected deque to be empty, but it's not")
-	}
-
-	// Test getLength
-	if d.getLength() != 0 {
-		t.Errorf("Expected deque length to be 0, but got %v", d.getLength())
-	}
-	d.enQueueFront(1)
-	if d.getLength() != 1 {
-		t.Errorf("Expected deque length to be 1, but got %v", d.getLength())
-	}
-	d.enQueueFront(2)
-	if d.getLength() != 2 {
-		t.Errorf("Expected deque length to be 2, but got %v", d.getLength())
-	}
-
 }
