1bd790043271520db61a355fa004ba4420cdae62
Aryan <1111aryantiwari@gmail.com>
2023-10-07 15:37:56 +0530

    docs: refactored comments to comply with go docs

--- strings/parenthesis/parenthesis.go
+++ strings/parenthesis/parenthesis.go
@@ -1,17 +1,12 @@
 package parenthesis
 
 // Parenthesis algorithm checks if every opened parenthesis
-// is closed correctly
-
-// when parcounter is less than 0 is because a closing
+// is closed correctly. Parcounter is less than 0 when a closing
 // parenthesis is detected without an opening parenthesis
-// that surrounds it
-
-// parcounter will be 0 if all open parenthesis are closed
-// correctly
+// that surrounds it and parcounter will be 0 if all
+// open parenthesis are closed correctly
 func Parenthesis(text string) bool {
 	parcounter := 0
-
 	for _, r := range text {
 		switch r {
 		case '(':
