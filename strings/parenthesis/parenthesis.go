858897f1e5d395d8c41d39b96336d8ba51c5a76b
Aryan <1111aryantiwari@gmail.com>
2023-10-10 21:15:57 +0530

    added full stop in the end

--- strings/parenthesis/parenthesis.go
+++ strings/parenthesis/parenthesis.go
@@ -4,7 +4,7 @@ package parenthesis
 // is closed correctly. When parcounter is less than 0 when a closing
 // parenthesis is detected without an opening parenthesis
 // that surrounds it and parcounter will be 0 if all open
-// parenthesis are closed correctly
+// parenthesis are closed correctly.
 func Parenthesis(text string) bool {
 	parcounter := 0
 
